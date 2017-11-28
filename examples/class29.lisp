e03d57f73142a07e622c0d17a9762d88a604fc70
azimut <azimut.github@protonmail.com>
2017-11-28 06:10:43 -0300

    add full glitch

--- examples/class29.lisp
+++ examples/class29.lisp
@@ -9,15 +9,17 @@
 (defvar *vert-stream* nil)
 
 (defparameter *texture* nil)
-(defparameter *sampler* nil)
+
+(defvar *offset* nil)
 
 (defun-g draw-verts-vert-stage ((vert :vec2))
   (values (v! vert 0 1)
           (+ (v2! 0.5) (* vert 0.5))))
 
 (defun-g draw-verts-frag-stage ((uv :vec2) &uniform (resolution :vec2)
-                                         (time :float)
-                                         (sam :sampler-2d))
+                                                    (time :float)
+                                (sam :sampler-2d)
+                                (offset :vec2))
   (let* ((st    (/ (s~ gl-frag-coord :xy)
                    (v! 1024. 768.) ))
 ;         (v     (* 5.0 0.4))
@@ -30,12 +32,20 @@
          (dt2   (* 10.  (y st) (step (+ 0.5 (* 0.5 (sin time)))
                                      (mod time 1.0))))
          (dx    100.)
+         (color (texture sam uv))
+         (color2 (v! (s~ (texture sam (+ uv (v2! (* dx (x offset)) 0.0)) ) :xyz)
+                     1.0))
+         (color3 (v! (s~ (texture sam (+ uv (v2! (* -100.0 (x offset)) 0.0)) ) :xyz)
+                     1.0))
          (minVX (step 0.2
                       (* 5.0 (mod (+ (x st) dt)  0.2))))
          (minVY (step 0.2
                       (* 5.0 (mod (+ (y st) dt2) 0.2))))
+         (color2 (mix color2 color minVX))
+         (color3 (mix color3 color2 minVY))
          )
-    (v! minVX 0.0 0.0 1.0)
+    color2
+;    (v! (x color2) (y color3) (x color2) 1.0)
   ))
 
 (defpipeline-g draw-verts-pipeline ()
@@ -54,7 +64,8 @@
    (map-g #'draw-verts-pipeline *vert-stream*
           :resolution (viewport-resolution (current-viewport))
           :time (now)
-          :sam *texture*)
+          :sam *texture*
+          :offset *offset*)
    (swap))
 
 (defun init ()
@@ -65,12 +76,6 @@
     (free *gpu-index-arr*))
   (when *vert-stream*
     (free *vert-stream*))
-
-  (when *texture*
-    (free *texture*))
-
-  (setf *texture*
-        (tex "patitos.jpg"))
   
   (setf *gpu-verts-arr*
         (make-gpu-array
@@ -79,6 +84,13 @@
                (v!  1.0 -1.0)
                (v!  1.0  1.0))
          :element-type :vec2))
+
+
+  (setf *texture* (tex "patitos.png"))
+
+  (setf *offset*
+      (v!  (/ 1.0 (first  (dimensions (sampler-texture *texture*))))
+           (/ 1.0 (second (dimensions (sampler-texture *texture*))))))
   
   (setf *gpu-index-arr*
         (make-gpu-array
