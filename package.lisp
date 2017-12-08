379281ab670a9355d321cd47a6ddebfa5627633b
azimut <azimut.github@protonmail.com>
2017-12-08 13:21:53 -0300

    whitelist more func

--- package.lisp
+++ package.lisp
@@ -3,18 +3,39 @@
 ;; vari to use dot
 
 (uiop:define-package somecepl
-    (:use #:cl #:cepl #:rtg-math
+  (:shadowing-import-from #:cepl #:free)
+  (:shadowing-import-from #:incudine #:buffer-data)
+  (:use #:cl #:cepl #:rtg-math
           #:nineveh
           #:vari
+          #:cl-ppcre
           #:incudine
           #:cepl.skitter
           #:livesupport)
-  (:shadowing-import-from #:cepl #:free)
-  (:shadowing-import-from #:incudine #:buffer-data)
-  (:import-from #:incudine.vug #:define-vug #:out #:~ #:sine #:dsp!)
+  (:import-from #:incudine.vug
+                #:define-vug
+                #:cout
+                #:buffer-play
+                #:foreach-channel
+                #:pan2
+                #:stereo
+                #:white-noise
+                #:samphold
+                #:make-local-adsr
+                #:phasor
+                #:bpf
+                #:buffer-read
+                #:butter-lp
+                #:fractal-noise
+                #:out
+                #:~
+                #:sine
+                #:dsp!)
   (:import-from #:incudine.util
                 #:with-samples
                 #:db->lin
                 #:non-negative-sample
-                #:lin->db #:*twopi-div-sr* #:+sample-zero+)
+                #:lin->db
+                #:*twopi-div-sr*
+                #:+sample-zero+)
 )
