ce79ccc66937e974248feb2760868b326aab106f
azimut <azimut.github@protonmail.com>
2017-12-17 22:44:11 -0300

    piano examples

--- examples/incudinepiano2.lisp
+++ examples/incudinepiano2.lisp
@@ -16,8 +16,11 @@
 (setf *ssdir* "/home/sendai/Downloads/morepiano/piano-sampler-incudine/")
 (setf *ssdir-f* "/home/sendai/Downloads/samples/pianosample/SalamanderGrandPianoV2_44.1khz16bit/44.1khz16bit/")
 
-(define-vug buffer-loop-play ((buffer buffer) rate start-pos
-                              loopstart loopend)
+(define-vug buffer-loop-play ((buffer buffer)
+                              rate
+                              start-pos
+                              loopstart
+                              loopend)
   (buffer-read buffer (phasor-loop rate start-pos loopstart loopend)
                :interpolation :cubic))
 
@@ -225,7 +228,7 @@ bouncing to disk:
                                                   (55 60 56)))))))
      (play-lsample-f (+ root -12) 1.9 .25)
      (at (+ (now) #[3/2 b]) #'play-lsample-f (+ -5  root) 1.9 .27)
-     (at (+ (now) #[5 b]) #'left newroot)))
+     (at (+ (now) #[4 b]) #'left newroot)))
 
 (defun right (&optional (oldtime 0) (newtime 0))
   ;(setf newtime (get-internal-real-time))
@@ -241,7 +244,6 @@ bouncing to disk:
 (defvar *myscale* nil)
 (setf *myscale* '(0 2 3 5 7 8 10) )
 (setf *myscale* '(0 1 3 5 6 8 10) )
-
 (defun right ()
   (play-lsample-f (qcosr *myscale* 60 7 3/4) 1. .2)
   (at (+ (now) #[.5 b]) #'right)
@@ -261,9 +263,278 @@ bouncing to disk:
   )
 
 
+(defvar *root* 0)
+(defvar *degree* 'i)
+
+(defun pchord ()
+  (play-lsample-f 60 1. .8)
+  (play-lsample-f 64 1. .8)
+  (play-lsample-f 67 1. .8)
+  )
+
+(pchord)
+(defun dancingl (dur)
+  (play-lsample-f 36
+                  (* dur (cosr .6 .3 1/7))
+                  (cosr .6 .3 1/7))
+  (aat (+ (now) #[2 b]) #'dancingl (random-list '(1 2/3))))
+
+(dancingl 4)
+
 #|
 (left)
 (incudine:free 0)
 (flush-pending)
 (right)
+(dancingl 2/3)
 |#
+
+#|
+src/overtone/examples/compositions/piano_phase.clj
+
+(defn player
+  [t speed notes]
+  (let [n      (first notes)
+        notes  (next notes)
+        t-next (+ t speed)]
+    (when n
+      (at t
+        (sampled-piano (note n)))
+      (apply-by t-next #'player [t-next speed notes]))))
+|#
+(defvar *piece* nil)
+(setf *piece* '(:E4 :F#4 :B4 :C#5 :D5 :F#4 :E4 :C#5 :B4 :F#4 :D5 :C#5))
+(setf (bpm *tempo*) 100)
+
+(defun player (time speed notes)
+  (let ((n      (first notes))
+        (notes  (cdr notes))
+        (t-next (+ time speed)))
+    (when n
+      (play-lsample-f (note-name-to-midi-number (symbol-name n)) 5.0 .9)
+      (at t-next #'player t-next speed notes))))
+
+(progn
+  (player (now) #[.338 b] (repeat 1000 *piece*))
+  (player (now) #[.335 b] (repeat 1000 *piece*)))
+
+;; --------------
+
+#|
+Extempore - An Overview
+https://vimeo.com/21956071
+at 11:30
+(define loop
+    (lambda (beat dur root)
+      (for-each (lambda (p offset)
+                  (play (+ offset) sampler p 100 (* 2.0 dur)))
+                (pc:make-chord 40 (cosr 75 10 1/32) 5
+                               (pc:chord root (if (member root '(10 8))
+                                                  '^7
+                                                  '-7)))
+                '(1/3 1 3/2 1 2 3))
+      (callback (*metro* (+ beat (* .5 dur))) 'loop (+ dur beat)
+                dur
+                (if (member root '(0 8))
+                    (random '(2 7 10))
+                    (random '(0 8))))))
+(loop (*metro* get-beat 4) 4 0)
+|#
+
+(defvar *beat-offset* nil)
+(setf *beat-offset* '(1/3 1 3/2 1 2 3))
+(setf (bpm *tempo*) 100)
+
+(defun sometune (dur root)
+  ;(at (+ (now) #[3 b]) #'play-lsample-f 36 5.0 1.0)
+  (mapcar (lambda (x y)
+            (at (+ (now) #[y b]) #'play-lsample-f x 10.0 1.0))
+          (make-chord 40
+                      (cosr 75 10 1/32)
+                      5
+                      (chord root (if (member root '(10 8))
+                                      '^7
+                                      '-7)))
+          *beat-offset*)
+  (at (+ (now) #[dur b]) #'sometune
+      dur
+      (if (member root '(0 8))
+          (random-list '(2 7 10))
+          (random-list '(0 8)))))
+
+(setf *beat-offset* (reverse *beat-offset*))
+
+(setf *beat-offset* '(0 0.1 1/3 0.7 0.9 0.9))
+(setf *beat-offset* '(0 0.2 1/3 0.5 0.8))
+(setf *beat-offset* '(0 0.2 0.4 0.6 0.8))
+(setf *beat-offset* '(0 0.1 0.2 0.3 0.4))
+(setf *beat-offset* '(0 0.1 0.11 0.13 0.15 0.17 0.2 0.4 0.5 0.55 0.6 0.8))
+
+(sometune 4 0)
+
+#|
+(incudine:free 0)
+(flush-pending)
+|#
+
+#|
+;; Ben S.
+;; A late christmas
+(define loop1
+    (lambda (beat dur)
+      (play piano 60 50 dur)
+      (callback (*metro* (+ beat (* .5 dur))) 'loop1 (+ beat dur) dur)))
+|#
+(defun loop1 (beat dur)
+  (play-lsample-f 60 dur .4)
+  (print beat)
+  (print dur)
+  (at (+ (now)
+         (+ #[beat b] (* .5 #[dur b]))) #'loop1 (+ beat dur) dur))
+
+(loop1 3 2)
+
+#|
+https://gist.github.com/CircuV/0834dbbd2034b82b7706
+
+(define chords
+  (lambda (time degree dur)
+    (println time degree dur)
+    (if (member degree '(i)) (set! dur 3))
+    (for-each 
+     (lambda (p)    
+     (play-midi-note 
+      (*metro* time)
+      *midi-out*
+      p
+      (real->integer (+ 50 (* 20 (cos (* pi time)))))
+      (*metro* 'dur dur)
+      0))
+
+    (pc:make-chord 50 70 2 (pc:diatonic 0 '- degree))
+    )
+    (callback (*metro* (+ time (* 0.5 dur))) 'chords (+ time dur)
+              (random (assoc degree '((i vii)
+                                      (vii i))))
+              dur)))
+|#
+
+(defvar *myscale* nil)
+(setf *myscale* (scale 0 'aeolian))
+(setf (bpm *tempo*) 100)
+
+(defun chords (degree dur)
+  (if (member degree '(i)) (setf dur 3))
+  (mapcar
+   (lambda (p) (play-lsample-f p 3.0 .4))
+   (make-chord 50 70 2 (diatonic 0 '- degree)))
+  (at (+ (now) #[dur b]) #'chords
+      (random-list (assoc degree '((i vii)
+                                   (vii i))))
+      dur))
+
+(chords 'i 3)
+
+#|
+
+(define chords
+  (lambda (time degree dur)
+    (if (member degree '(i)) (set! dur 3.0))
+    (println time degree dur)
+    (for-each (lambda (p)
+    (let* ((dur1 (* dur (random '(0.5 1))))
+           (dur2 (- dur dur1)))
+    (play-midi-note (*metro* time) *midi-out* p 
+          (real->integer(+ 50 (* 20 (cos (* pi time)))))
+          (*metro* 'dur dur1) 0)
+    (if (> dur2 0)
+      (play-midi-note (*metro* (+ time dur1)) *midi-out*
+          (pc:relative p (random '(-2 -1 1 2))
+                       (pc:scale 0 'aeolian))  
+          (real->integer (+ 50 (* 20 (cos (* pi (+ time dur1))))))
+          (*metro* 'dur dur2) 0 ))))
+
+    (pc:make-chord 50 70 2 (pc:diatonic 0 '- degree)))
+    (callback (*metro* (+ time (* 0.5 dur))) 'chords (+ time dur)
+              (random (assoc degree '((i vii)
+                                      (vii i))))
+              (random (list 1 2 3)))))
+
+|#
+(defun chords (degree dur)
+;  (if (member degree '(i)) (setf dur 3.0))
+  (if (member degree '(i)) (setf dur (random-list '(3.0 6.0))))
+  ; 4:00
+  (let* ((nnote (car (diatonic 0 '- degree))))
+    ;; Hack needed when degree returns 2 , which is converted by pcrrandom in -1
+    (if (= nnote 2) (setf nnote (cadr (diatonic 0 '- degree))))
+    (play-lsample-f (pcrrandom 40 50 (list nnote))
+                  dur .4))
+  (mapcar
+   (lambda (p) (let* ((dur1 (* dur (random-list '(0.5 1))))
+                      (dur2 (- dur dur1)))
+                 (play-lsample-f p dur1 .4)
+                 (if (> dur2 0)
+                     (at (+ (now) #[dur1 b]) #'play-lsample-f
+                         (relative p (random-list '(-2 -1 1 2)) (scale 0 'aeolian))
+                         dur1
+                         .4))))
+ ;   (make-chord 50 70 2 (diatonic 0 '- degree)))
+  (make-chord 50 70 3 (diatonic 0 '- degree)))
+  (at (+ (now) #[dur b]) #'chords
+;      (random-list (assoc degree '((i vii)
+;                                   (vii i))))
+      ;; (random-list (assoc degree '((i vii)
+      ;;                              (vii i v)
+      ;;                              (v i))))
+      (random-list (assoc degree '((i vii)
+                                   (vii i v)
+                                   (v i vi)
+                                   (vi ii)
+                                   (ii v vii))))
+      (random-list '(1 2 3))))
+
+
+
+;; ----------------------
+;; DANCING PHALANGES
+(setf (bpm *tempo*) 110)
+(defvar *root* nil)
+(setf *root* 0)
+(defvar *degree* nil)
+(setf *degree* 'i)
+(defvar *scale* nil)
+(setf *scale* (scale 0 'phrygian))
+
+(defun melody (ms rs)
+  (if (not ms)
+      'done
+      (let ((p (car ms)))
+        (play-lsample-f (quant (+ 48 *root* p) *scale*)
+                        (* (car rs) (cosr .9 .2 1/2)) .9)
+        (play-lsample-f (quant (+ 55 *root* p) *scale*)
+                        (* (car rs) (cosr .9 .2 1/2)) .9)
+        (at (+ (now) #[(car rs) b]) #'melody (cdr ms) (cdr rs))
+        )))
+
+(defun right (degree)
+  (setf *degree* degree)
+  (setf *root* (car (diatonic 0 '- degree)))
+  (at (+ (now) #[4 b]) #'right (random-list (cdr (assoc degree '((i n6 iii vii)
+                                                                 (n6 v7)
+                                                                 (iii v7)
+                                                                 (vii vi)
+                                                                 (vi vii v7 v7)
+                                                                 (v7 i))))))
+  )
+
+(right 'i)
+
+(defun left (dur)
+  (if (> (random 1.0) .85) (melody '(0 1 0 -1) '(1/3 2/3 2/3 1)))
+  (play-lsample-f (+ 48 *root*) (* dur (cosr .9 .3 1/7)) (cosr .6 .3 1/2))
+  (play-lsample-f 36 (* dur (cosr .9 .3 1/7)) (cosr .6 .3 1/2))
+  (at (+ (now) #[dur b]) #'left (random-list '(1 2/3))))
+
+(left 2/3)
+(flush-pending)
