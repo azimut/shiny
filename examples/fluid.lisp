a28d094b668682bab4f59f36d045ef3b567bce12
azimut <azimut.github@protonmail.com>
2018-01-15 03:12:43 -0300

    clean up molecular

--- examples/fluid.lisp
+++ examples/fluid.lisp
@@ -27,8 +27,8 @@
 ;(fluidsynth:sfload *synth* "/home/sendai/Downloads/fluid-soundfont-3.1/FluidR3_GM.sf2" 1)
 ;(fluidsynth:sfload *synth* "/usr/share/sounds/sf2/FluidR3_GM.sf2" 1)
 ;(fluidsynth:sfload *synth* "/home/sendai/Downloads/samples/GeneralUser GS 1.471/GeneralUser GS v1.471.sf2" 1)
-(fluidsynth:sfload *synth* "/home/sendai/Downloads/Sonatina_Symphonic_Orchestra.sf2" 1)
-;(fluidsynth:sfload *synth* "/home/sendai/Downloads/Nice-Keys-Ultimate-V2.3.sf2" 1)
+;(fluidsynth:sfload *synth* "/home/sendai/Downloads/Sonatina_Symphonic_Orchestra.sf2" 1)
+(fluidsynth:sfload *synth* "/home/sendai/Downloads/Nice-Keys-Ultimate-V2.3.sf2" 1)
 
 #|
 (fluidsynth:get-active-voice-count *synth*)
@@ -394,218 +394,10 @@
 ;; https://github.com/igorski/molecular-music-generator
 ;; --------------------------------------------------------------------
 
-
-
-;; (defun mpattern (time lpattern notes length1 length2 &key accumnotes accumtempos (counter 0) (swap "N"))
-;;   (if (not (null notes))
-;;       (let* ((counter (1+ counter))
-;;              (next    (tempo-sync #[length1 b]))
-;;              (neext   (tempo-sync #[length2 b]))
-;;              (note    (first notes))
-;;              (realnext nil)
-;;              (accumnotes  (append  accumnotes (list note))))
-;;         (play-midi-note time note 50 1 (random 9))
-
-;; ;;        (print swap)
-;;         (if (or (equal swap "Y") (= counter lpattern)) (setf realnext neext) (setf realnext next))
-;;         (if (numberp (position realnext *mtempos*)) (setf swap "Y"))
-;; ;;        (print swap)
-
-;;         (cond ((= counter lpattern)
-;;                (progn
-;;                  (aat next  #'spattern it accumnotes (append accumtempos (list length1)) (random 11))
-;;                  (aat neext #'mpattern it length2 (cdr notes) length2 length1 :counter 0)))
-;;               ((equal swap "Y")
-;;                (progn
-;;                  (print "swapping it up")
-;;                  (aat neext #'mpattern
-;;                       it lpattern (cdr notes) length2 length1
-;;                       :accumnotes  accumnotes
-;;                       :accumtempos (append accumtempos (list length1))
-;;                       :counter counter)))
-;;               (t 
-;;                (aat next #'mpattern
-;;                     it lpattern (cdr notes) length1 length2
-;;                     :accumnotes  accumnotes
-;;                     :accumtempos (append accumtempos (list length1))
-;;                     :counter counter
-;;                     :swap swap))))))
-
-;; (defun mpattern (time lpattern notes length1 length2 &key accumnotes accumtempos (counter 0) (swap "N"))
-;;   (if (not (null notes))
-;;       (let* (
-;;              (counter (+ counter length1))
-;;              (next    (tempo-sync #[length1 b]))
-;;              (neext   (tempo-sync #[length2 b]))
-;;              (note    (first notes))
-;;              (realnext nil)
-;;              (accumnotes  (append  accumnotes (list note))))
-;;         (play-midi-note time note 50 1 11)
-
-;;         (print swap)
-;;         ;;(if (or (equal swap "Y") (>= counter lpattern)) (setf realnext neext) (setf realnext next))
-;;         (if (numberp (position next *mtempos*)) (setf swap "Y"))
-;;         (print swap)
-
-;;         (cond ((>= counter lpattern)
-;;                (progn
-;;                  (aat next #'spattern it accumnotes (append accumtempos (list length1)) (random 11))
-;;                  (aat next #'mpattern it lpattern (cdr notes) length1 length2)))
-;;               ((equal swap "Y")
-;;                (progn
-;;                  (print "swapping it up")
-;;                  (aat neext #'mpattern
-;;                       it lpattern (cdr notes) length2 length1
-;;                       :accumnotes  accumnotes
-;;                       :accumtempos (append accumtempos (list length1))
-;;                       :counter counter )))
-;;               (t 
-;;                (aat next #'mpattern
-;;                     it lpattern (cdr notes) length1 length2
-;;                     :accumnotes  accumnotes
-;;                     :accumtempos (append accumtempos (list length1))
-;;                     :counter counter
-;;                     :swap swap))))))
-
-
-;; (defun spattern (time notes tempos pattern r) 
-;;   (let* ((note1 (cm:next notes))
-;;          (note2 (cm:next notes))
-;;          (pat1 (cm:next pattern))
-;;          (pat2 (cm:next pattern))
-;;          (next (tempo-sync #[1 b]))
-;;          (neext (tempo-sync #[2 b])))
-;;     ;;(setf *mtempos* (append (list next) *mtempos*))
-;;     (if (= pat1 1)
-;;         (play-midi-note time note1 60 1 (random 11)))
-;;     (if (= pat2 1)
-;;         (play-midi-note next note2 60 1 (random 11)))
-;;     (aat neext #'spattern
-;;          it
-;;          notes
-;;          (alexandria:rotate tempos -1)
-;;          pattern
-;;          r)))
-
-;; (defun spattern (time notes tempos pattern r)
-;;   (let* ((note1 (first notes))
-;;          (pat1  (first pattern))
-;;          (pat2  (second pattern))
-;;          (next  (tempo-sync #[1 b])))
-;;     (if (= pat1 1)
-;;         (progn
-;;           (print "now1")
-;;           (play-midi-note (now) note1 60 1 (random 11))))
-;;     (if (= pat2 1)
-;;         (progn
-;;           (print "now2")
-;;           (setf *mtempos* (append (list (round next)) *mtempos*))))
-;;     (aat next #'spattern
-;;          it
-;;          (rotate-list notes -1)
-;;          (rotate-list tempos -1)
-;;          (rotate-list pattern -1)
-;;          r)))
-  
-;; (defun mpattern (time lpattern notes length1 length2 &key accumnotes accumtempos accumbeats (counter 0) (beat 0) (swap "N"))
-;;   (if (not (null notes))
-;;       (let* (
-;;              (counter (+ counter length1))
-;;              (next    (tempo-sync #[length1 b]))
-;;              (neext   (tempo-sync #[length2 b]))
-;;              (realnext nil)
-;;              (note    (first notes))
-;;              (accumnotes (append accumnotes (list note)))
-;;              (accumbeats (append accumbeats '(1) (repeat (- length1 1) '(0)))))
-        
-;;         (play-midi-note time note 60 .5 (random 11))
-;;         (print swap)
-;;         (print length1)
-
-       
-;;         ;; ;; Predict if new pattern to be created is going to play next beat
-;;         (if  accumbeats)) (equal swap "Y") (>= (+ length2 counter) lpattern))
-;;               ((and (= 1 (first accumbeats)) (equal swap "N") (>= (+ length1 counter) lpattern))
-;;                  (setf *mtempos* (append *mtempos* (list next))))))
-        
-;;         (if (numberp (position (round next) *mtempos*))
-;;             (setf swap "Y"))
-        
-;;         (cond ((>= counter lpattern)
-;;                (progn
-;;                  (print "end patern")
-;;                  (print accumtempos)
-;;                  (print accumbeats)
-;;                  (aat time #'spattern
-;;                       it
-;;                       accumnotes
-;;                       (append accumtempos (list length1))
-;;                       accumbeats
-;;                       (random 11))
-;;                  ;; (setf accumbeats nil)
-;;                  ;; Star a new pattern
-;;                  (if (equal swap "Y")
-;;                      (aat next #'mpattern it lpattern (cdr notes) length2 length1)
-;;                      (aat next #'mpattern it lpattern (cdr notes) length1 length2))))
-;;              ((equal swap "Y")
-;;                (progn
-;;                  (print "swapping it up")
-;;                  (aat neext #'mpattern
-;;                       it lpattern (cdr notes) length2 length1
-;;                       :accumnotes  accumnotes
-;;                       :accumtempos (append accumtempos (list length1))
-;;                       :accumbeats accumbeats
-;;                       :counter counter
-;;                       :beat (+ beat length1))))
-;;               (t
-;;                (progn
-;;                  (print "what")
-;;                  (aat next #'mpattern
-;;                       it lpattern (cdr notes) length1 length2
-;;                       :accumnotes  accumnotes
-;;                       :accumtempos (append accumtempos (list length1))
-;;                       :accumbeats accumbeats
-;;                       :counter counter
-;;                       :swap swap
-;;                       :beat (+ beat length1))))))))
-
 (defvar *mtempos* nil)
 (setf *mtempos* nil)
 (setf (bpm *tempo*) 80)
 
-;; (defun spattern (time notes pattern r)  
-;;   (let (
-;;          (pat1  (nth 1 pattern))
-;;          (pat2  (nth 2 pattern))
-;;          (pat3  (nth 3 pattern))
-;;          (pat4  (nth 4 pattern))
-;;          (pat5  (nth 5 pattern))
-;;          (next1  (tempo-sync #[1 b]))
-;;          (next2  (tempo-sync #[2 b]))
-;;          (next3  (tempo-sync #[3 b]))
-;;          (next4  (tempo-sync #[4 b])))
-;;     (if (= pat1 1)
-;;           (play-midi-note time (cm:next notes) 60 1 (random 11)))
-;;     (if (= pat2 1)
-;;         (progn
-;;           (play-midi-note next1 (cm:next notes) 60 1 (random 11))
-;;           (setf *mtempos* (append (list next1) *mtempos*))))
-;;     (if (= pat3 1)
-;;         (progn
-;;           (play-midi-note next2 (cm:next notes) 60 1 (random 11))
-;;           (setf *mtempos* (append (list next2) *mtempos*))))
-;;     (if (= pat4 1)
-;;         (progn
-;;           (play-midi-note next3 (cm:next notes) 60 1 (random 11))
-;;           (setf *mtempos* (append (list next3) *mtempos*))))
-;;     (if (= pat5 1)
-;;         (setf *mtempos* (append (list next4) *mtempos*)))
-;;     (aat next4 #'spattern
-;;          it
-;;          notes
-;;          (rotate-list pattern -4)
-;;          r)))
-
 (defun spattern (time notes pattern r)
   (let* ((lpattern   (length pattern))
          (t-lpattern (tempo-sync #[(/ lpattern 2) b]))
@@ -615,108 +407,13 @@
                                :collect (if (= 1 beat) nbeat)))))
     (loop :for cbeat :in pbeat :do
         (if (= cbeat 0)
-            (play-midi-note time (cm:next notes) 60 1 (random 11))
+            (play-midi-note time (cm:next notes) 30 1 (random 8))
             (let ((t-beat (tempo-sync #[(/ cbeat 2) b])))
-              (play-midi-note t-beat (cm:next notes) 60 1 (random 11))
-              (setf *mtempos* (append *mtempos* (list t-beat))))))
-    (if (= 1 (first pattern))
-        (setf *mtempos* (append *mtempos* (list t-lpattern))))
-    (aat t-lpattern #'spattern it notes pattern r)))
-
-
-(defun spattern (time notes pattern r)
-  (let* ((lpattern   (length pattern))
-         (t-lpattern (tempo-sync #[(/ lpattern 2) b]))
-         (pbeat      (remove nil
-                            (loop :for beat :in pattern
-                               :for nbeat :from 0 :upto 64
-                               :collect (if (= 1 beat) nbeat)))))
-    (print pattern)
-    (loop :for cbeat :in pbeat :do
-        (if (not (= cbeat 0))
-;;            (play-midi-note time (cm:next notes) 60 1 (random 11))
-            (let ((t-beat (tempo-sync #[(/ cbeat 2) b])))
-;;              (play-midi-note t-beat (cm:next notes) 60 1 (random 11))
-              (setf *mtempos* (append *mtempos* (list t-beat))))))
-    (if (= 1 (first pattern))
-        (setf *mtempos* (append *mtempos* (list t-lpattern))))
-    (aat t-lpattern #'spattern it notes pattern r)))
-
-(defun spattern (time notes pattern r)
-  (let* ((lpattern   (length pattern))
-         (t-lpattern (tempo-sync #[(/ lpattern 2) b]))
-         (pbeat      (remove nil
-                            (loop :for beat :in pattern
-                               :for nbeat :from 0 :upto 64
-                               :collect (if (= 1 beat) nbeat)))))
-    (loop :for cbeat :in pbeat :do
-        (if (= cbeat 0)
-            (play-midi-note time (cm:next notes) 30 .7 (random 11))
-            (let ((t-beat (tempo-sync #[(/ cbeat 2) b])))
-              (play-midi-note t-beat (cm:next notes) 30 .7 (random 11))
+              (play-midi-note t-beat (cm:next notes) 30 1 (random 8))
               (setf *mtempos* (append *mtempos* (list cbeat))))))
     (if (= 1 (first pattern))
         (setf *mtempos* (append *mtempos* (list 0))))
     (aat t-lpattern #'spattern it notes pattern r)))
-
-
-
-;; ;; I need to use (mod) to get the next beat where there is a note
-;; (defun ppattern (time lpattern notes length1 length2 &key
-;;                                                        (cbeat 0)
-;;                                                        (ibeat 0)
-;;                                                        accumbeats accumnotes play pbeat)
-;;   (if (not (null notes))
-;;       (let ((nbeat   (+ .5 cbeat))
-;;             (nibeat  (+  1 ibeat))
-;;             (t-nbeat (tempo-sync #[.5 b]))
-;;             (note    (first notes))
-;;             (swap    nil))
-;;         ;; play now
-;;         (cond ((or (equal play "yes")
-;;                    (= pbeat ibeat))
-;;                (progn
-;;                  (play-midi-note time note 60 1 (random 11))
-;;                  (setf notes      (cdr notes))
-;;                  (setf accumnotes (append accumnotes (list note)))
-;;                  (setf accumbeats (append accumbeats '(1)))
-;;                  (setf pbeat      (mod (+ ibeat (* length1 2)) lpattern))
-;;                  (if (numberp (position (tempo-sync #[length1 b]) *mtempos*))
-;;                      (setf swap "y"))))
-;;               (t
-;;                (setf accumbeats (append accumbeats '(0)))))
-;;         ;; reset when the next .5 beat is the last beat of the pattern
-;;         ;; if not is business as usual and we stick with this pattern
-;;         (if (= lpattern nibeat)
-;;             (progn
-;;               (print "endpattern")
-;;               (print length1)
-;;               (print accumbeats)
-;;               (aat t-nbeat #'spattern it
-;;                    (cm:new cm:cycle :of accumnotes) accumbeats 1)
-;;               ;; This works to match agains the prev NOT the global
-;;               (if (= 1 (first accumbeats))
-;;                   (progn
-;;                     (print "swap")
-;; ;;                    (setf *mtempos* (append *mtempos* (list t-nbeat)))
-;;                     (aat t-nbeat #'ppattern it lpattern notes length2 length1 :pbeat pbeat))
-;;                   (aat t-nbeat #'ppattern it lpattern notes length1 length2 :pbeat pbeat)))
-;;             (if (equal swap "y")
-;;                 (progn
-;;                   (aat t-nbeat #'ppattern it lpattern notes length2 length1
-;;                        :accumnotes accumnotes
-;;                        :accumbeats accumbeats
-;;                        :cbeat nbeat
-;;                        :ibeat nibeat
-;;                        :pbeat pbeat)
-;;                   (print "other swap"))
-;;                 (aat t-nbeat #'ppattern it lpattern notes length1 length2
-;;                      :accumnotes accumnotes
-;;                      :accumbeats accumbeats
-;;                      :cbeat nbeat
-;;                      :ibeat nibeat
-;;                      :pbeat pbeat))))))
-
 
 ;; I need to use (mod) to get the next beat where there is a note
 (defun ppattern (time lpattern notes length1 length2 &key
@@ -784,18 +481,12 @@
 (defun mbox (time lpattern note length1 length2 pc)  
   (setf *mtempos* nil)
   (let* ((midinote (cm:keynum note))
-         (notes    (loop for x from -14 to 21 collect (relative midinote x pc))))
+         (notes    (loop for x from -14 to 14 collect (relative midinote x pc))))
     (ppattern time lpattern notes length1 length2 :play "yes")))
 
 (mbox (tempo-sync #[1 b]) 32 :E 4 3 (scale 0 'dorian))
-(mbox (now) 16 :E 4 3 (scale 0 'dorian))
-
 (mbox (tempo-sync #[1 b]) 32 :C 9 14.5 (scale 0 'dorian))
 (mbox (tempo-sync #[1 b]) 32 :G 10 3.5 (scale 0 'dorian))
-
-
-(mbox (now) 32 :C 9 14.5 (scale 0 'dorian))
-(mbox (now) 32 :G 10 3.5 (scale 0 'dorian))
 
 (flush-pending)
 (mapcar (lambda (x) (fluidsynth:noteoff *synth* 11 x)) (range 100))
