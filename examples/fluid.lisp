7df2e275661322f9c93b20a20c628138f1abda5f
azimut <azimut@example.com>
2018-02-14 21:27:31 -0300

    up jazz

--- examples/fluid.lisp
+++ examples/fluid.lisp
@@ -1786,7 +1786,7 @@ h half   x sixty-fourth
 ;;;
 
 (defun play-midi-note (time pitch velocity dur c)
-  (when (> dur 0)
+  (when (> pitch 0)
     (progn
       (at time #'fluidsynth:noteon
           *synth* c pitch velocity)
@@ -1828,10 +1828,7 @@ h half   x sixty-fourth
                      ,cm::+acoustic-bass-drum+))
                  't)))
   (when (null rhythms)
-    (setf rhythms (cm:next
-                   (cm:new cm:cycle
-                     :of (cm:rhythm '(t4 t8) 60))
-                   't)))
+    (setf rhythms (cm:rhythm '(t4 t8 t4 t8 t4 t8) 30)))
 
   (when (null amps)
     (setf amps (cm:next
@@ -1850,6 +1847,7 @@ h half   x sixty-fourth
 ;; wt
 ;; is weight of resting relative to playing
 ;; return weighting pattern that slightly prefers
+
 ;; playing a ride 1 pattern
 ;;    over a ride 2 pattern
 (defun or12r (wt)
@@ -1868,33 +1866,27 @@ h half   x sixty-fourth
   (mapcar
    #'round
    (mapcar
-    (lambda (x) (cm:interp x 0.0 20 1.0 60))
+    (lambda (x) (cm:interp x 0.0 10 1.0 40))
     (cm:amplitude amps))))
 
-
-(defun jazz-cymbals (time &optional notes amps))
+;;  1  -  x    1  -  1    1  x  x    1  x  1
+(defun jazz-cymbals (time rhythm &optional notes amps)
   (when (null notes)
-    (setf notes (cm:next
-                 (cm:new cm:cycle
-                   :of (list cm::+ride-cymbal-1+ 0 (or12r 5)
-                             cm::+ride-cymbal-1+ 0 cm::+ride-cymbal-1+
-                             cm::+ride-cymbal-1+ (or12r 7) (or12r 7)
-                             cm::+ride-cymbal-1+ (or12r 3) cm::+ride-cymbal-1+                             )
-                   )
-                 't)))
+    (setf notes (cm:next (cm:new cm:cycle :of (list
+                 cm::+ride-cymbal-1+ 0 (or12r 5)
+                 cm::+ride-cymbal-1+ 0 cm::+ride-cymbal-1+
+                 cm::+ride-cymbal-1+ (or12r 7) (or12r 7)
+                 cm::+ride-cymbal-1+ (or12r 3) cm::+ride-cymbal-1+)) 't)))
   (when (null amps)
-    (setf amps (cm:next
-                (cm:new cm:cycle
-                  :of (lamps '(:mf :mp :fff :f
-                               :mp :ffff :mf :mp
-                               :fff :f :mp :ffff)))
-                't)))
+    (setf amps (lamps '(:mf :mp :fff :f
+                        :mp :ffff :mf :mp
+                        :fff :f :mp :ffff))))
+  (play-midi-note time (first notes) (first amps) rhythm 8)
+  (aat (+ time #[rhythm b]) #'jazz-cymbals
+       it rhythm (rest notes) (rest amps)))
 
-  (play-midi-note time (first notes) (first amps) .33 8)
-  (aat (+ time #[0.33 b]) #'jazz-cymbals
-       it (rest notes) (rest amps)))
-
-(jazz-cymbals (tempo-sync #[1 b]))
+(fluidsynth:program-change *synth* 8 1)
+(jazz-cymbals (tempo-sync #[1 b]) (cm:rhythm 't8 60))
 
 ;;;
 ;; <3
@@ -1916,18 +1908,20 @@ h half   x sixty-fourth
 
   (when (null rhythms)
     (if (null (cm:odds .3))
-        (setf rhythms '(1 1 1 1 1 1 1 1 1))
-;;        (setf rhythms (cm:rhythm '(t8 t8 t8 t8 t8 t8 t8 t8) 60))
-        (setf rhythms (cm:rhythm '(t4 t8 t4 t8) 60))))
+        (setf rhythms (repeat 8 '(1)))
+;;        (setf rhythms (cm:rhythm (repeat 8 '(t8)) 60))
+        (setf rhythms (cm:rhythm '(t4 t8 t4 t8 t4 t8 t4 t8) 60))))
   
   (let ((rhythm (first rhythms)))
     (play-midi-note
      time (cm:keynum
-           (cm:transpose (first notes) root)) 30 rhythm 1)
+           (cm:transpose (first notes) root)) (round (cosr 26 3 3/4)) rhythm 1)
     (aat (+ time #[rhythm b])
          #'jazz-piano
          it (rest notes) (rest rhythms) root)))
 
-(jazz-piano (tempo-sync #[1 b]))
+(jazz-high-hat (tempo-sync #[1 b]))
+(jazz-cymbals  (tempo-sync #[1 b]) (cm:rhythm 't8 60))
+(jazz-piano    (tempo-sync #[1 b]))
 
-(flush-pending)
+(fluidsynth:program-change *synth* 1 48)
