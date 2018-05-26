0f4ee90fa56b31a17d64f34d7d3c9c3f18d97396
azimut <azimut.github@protonmail.com>
2018-05-26 09:02:07 -0300

    simplify jazz example from cm

--- compositions/jazz.lisp
+++ compositions/jazz.lisp
@@ -33,115 +33,109 @@
   (play-midi-note time (first notes) (round (cosr 35 5 1/2)) .33 1)
   (aat (+ time #[1 b]) #'jazz-high-hat it (rest notes)))
 |#
-
+(defun jazz-high-hat ())
 (defun jazz-high-hat (time)
-  (play-midi-note time *gm-closed-hi-hat* (round (cosr 35 3 1/2)) .33 0)
-  (aat (+ time #[2 b]) #'jazz-high-hat it))
+  (p time 42 (rcosr 25 3 1/2) 1/3 0)
+  (aat (+ time 2) #'jazz-high-hat it))
 
-(jazz-high-hat (tempo-sync #[1 b]))
+(jazz-high-hat (quant 4))
 
-(defun jazz-drums (time &optional notes rhythms amps)
-  (when (null notes)
-    (setf notes (cm:next
-                 (cm:new cm:weighting
-                   :of
-                   `((0 :weight .25)
-                     ,cm::+electric-snare+
-                     ,cm::+acoustic-bass-drum+))
-                 't)))
-  (when (null rhythms)
-    (setf rhythms (cm:rhythm '(t4 t8 t4 t8 t4 t8) 30)))
+(defbeat hithat 1 "x-x-"
+  (p time 42 (rcosr 20 3 1/2) 1/3 0))
 
-  (when (null amps)
-    (setf amps (cm:next
-                (cm:new cm:weighting
-                  :of '(31 (40 :weight .1)))
-                't)))
+(hithat)
+(defun %hithat ())
 
-  (let ((rhythm (first rhythms)))
-    (play-midi-note
-     time (first notes) (first amps) rhythm 7)
-    (aat (+ time #[rhythm b]) #'jazz-drums
-         it (rest notes) (rest rhythms) (rest amps))))
+(let ((notes   (make-weighting
+                 '((0 :weight .25)
+                   40
+                   35)))
+      (rhythms (make-cycle '(2/3 4/3)))
+      (amps    (make-weighting '(21 (30 :weight .1)))))
+  (defun jazz-drums (time)   
+    (let ((rhythm (next rhythms)))
+      (p
+       time (next notes) (next amps) rhythm 0)
+      (aat (+ time rhythm) #'jazz-drums
+           it))))
 
-(jazz-drums (tempo-sync #[1 b]))
+(defun jazz-drums ())
+(jazz-drums (quant 4))
+(hithat)
 
 ;; wt
 ;; is weight of resting relative to playing
 ;; return weighting pattern that slightly prefers
 
-;; playing a ride 1 pattern
-;;    over a ride 2 pattern
+;; playing a ride 1 pattern - 51
+;;    over a ride 2 pattern - 59
 (defun or12r (wt)
-  (cm:new cm:weighting
-    :of (list
-         (list
-          (cm:new cm:weighting
-            :of `(51 (0 :weight ,wt))
-            :for 1) :weight 1.5)
-          (cm:new cm:weighting
-            :of `(59 (0 :weight ,wt))
-            :for 1))
-    :for 1))
+  (make-weighting
+   (list
+    (list
+     (make-weighting `(51 (0 :weight ,wt)) 1)
+     :weight 1.5)
+    (make-weighting `(59 (0 :weight ,wt)) 1))
+   2))
 
 (defun lamps (amps)
   (mapcar
    #'round
    (mapcar
-    (lambda (x) (cm:interp x 0.0 10 1.0 40))
+    (lambda (x) (cm:interp x 0.0 30 1.0 70))
     (cm:amplitude amps))))
 
-;;  1  -  x    1  -  1    1  x  x    1  x  1
+; Triplet 8th: 1  2  3    4  5  6    7  8  9   10 11 12
+; Cymbals:     1  -  x    1  -  1    1  x  x    1  x  1 
 (defun jazz-cymbals (time rhythm &optional notes amps)
-  (when (null notes)
-    (setf notes (cm:next (cm:new cm:cycle :of (list
-                 cm::+ride-cymbal-1+ 0 (or12r 5)
-                 cm::+ride-cymbal-1+ 0 cm::+ride-cymbal-1+
-                 cm::+ride-cymbal-1+ (or12r 7) (or12r 7)
-                 cm::+ride-cymbal-1+ (or12r 3) cm::+ride-cymbal-1+)) 't)))
+  (setp notes (next (new cycle
+                      :of (list
+                           51 0 (or12r 5)
+                           51 0 51
+                           51 (or12r 7) (or12r 7)
+                           51 (or12r 3) 51))
+                    't))
   (when (null amps)
     (setf amps (lamps '(:mf :mp :fff :f
                         :mp :ffff :mf :mp
                         :fff :f :mp :ffff))))
-  (play-midi-note time (first notes) (first amps) rhythm 8)
-  (aat (+ time #[rhythm b]) #'jazz-cymbals
+  (p time (first notes) (+ -20 (first amps)) rhythm 0)
+  (aat (+ time rhythm) #'jazz-cymbals
        it rhythm (rest notes) (rest amps)))
 
-(fluidsynth:program-change *synth* 8 1)
-(jazz-cymbals (tempo-sync #[1 b]) (cm:rhythm 't8 60))
+(jazz-cymbals (quant 4) (cm:rhythm 't8 60))
+(defun jazz-cymbals ())
 
 ;;;
 ;; <3
 ;;;
 
 (defun jazz-piano (time &optional notes rhythms root)
-
   (when (null notes)
     (setf root (random-elt *jazz-changes*))
-    (setf notes (cm:next
-                 (cm:new cm:weighting
-                   :of `(0 (,(cm:new cm:heap
-                               :of *jazz-scale*
-                               :for (cm:new cm:weighting
-                                      :of '(1 2 3 4)))
-                             :weight ,(cm:new cm:weighting
-                                        :of '(1.15 1.65)))))
+    (setf notes (next
+                 (make-weighting
+                  (list
+                   (list (make-heap *jazz-scale* (make-weighting '(1 2 3 4)))
+                         :weight (make-weighting '(1.5 1.65)))
+                   0))
                  't)))
-
   (when (null rhythms)
-    (if (null (cm:odds .2))
-        (setf rhythms (repeat 8 '(1)))
+    (if (null (odds .65))
+        (setf rhythms (repeat 8 '(2/3 1/3)))
+        (setf rhythms (repeat 8 '(1/3)))
+;;        (setf rhythms (repeat 8 '(1)))
 ;;        (setf rhythms (cm:rhythm (repeat 8 '(t8)) 60))
-        (setf rhythms (cm:rhythm '(t4 t8 t4 t8 t4 t8 t4 t8) 60))))
-  
+;;        (setf rhythms (cm:rhythm '(t4 t8 t4 t8 t4 t8 t4 t8) 60))
+        ))  
   (let ((rhythm (car rhythms)))
-    (play-midi-note
+    (p
      time
      (cm:keynum (cm:transpose (car notes) root))
-     (round (cosr 30 3 3/4))
+     (round (cosr 50 3 3/4))
      rhythm
-     1)
-    (aat (+ time #[rhythm b])
+     11)
+    (aat (+ time rhythm)
 
          #'jazz-piano
          it
@@ -149,9 +143,13 @@
          (cdr rhythms)
          root)))
 
+(fp 11 62)
+
 (jazz-high-hat (tempo-sync #[1 b]))
 (jazz-cymbals  (tempo-sync #[1 b]) (cm:rhythm 't8 60))
-(jazz-piano    (tempo-sync #[1 b]))
+(jazz-piano    (quant 4))
+(defun jazz-piano ())
+
 
 (jazz-high-hat (funcall *metro* (funcall *metro* 'get-beat 4)) )
 (jazz-cymbals  (funcall *metro* (funcall *metro* 'get-beat 2)) (cm:rhythm 't8 60))
