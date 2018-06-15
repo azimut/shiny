1489e1883686ce1bd1ca8643208cdab06b2a37a1
azimut <azimut.github@protonmail.com>
2018-06-15 02:39:13 -0300

    add notalent code

---
+++ compositions/notalent.lisp
@@ -0,0 +1,133 @@
+(in-package :somecepl)
+
+;; some inspiration from jazz.lisp
+
+(fp 0 10)
+(fp 1 20)
+(fp 4 11)
+(fp 3 20)
+
+(fg 2.0)
+
+(fp 0 0)
+(fp 1 11)
+
+(defvar *notes* nil)
+
+(defun spawn-circle (pitch life
+                     &key growth (pos (v2! 0)) (color (v3! 0))
+                       (shape 0)
+                       (selfcolor (v3! 1))
+                       (pencil 1)
+                       (rotation 0f0)
+                       (fade 0))
+  (let ((mypitch pitch))
+    (when (eq 'cons (type-of mypitch))
+      (setf mypitch (first mypitch)))
+    (when (> mypitch 0)
+      (push (make-instance 'circle-note
+                           :pos pos
+                           :pencil pencil
+                           :color color
+                           :selfcolor selfcolor
+                           :shape shape
+                           :growth growth
+                           :life life
+                           :die-at (+ (get-universal-time)
+                                      (round life))
+                           :pitch mypitch
+                           :rotation rotation
+                           :fade (if (= 1 fade) (float life) -1f0))
+            *notes*)))
+  pitch)
+
+(fg 1.0)
+
+;;--------------------------------------------------
+
+(let ((notes (make-cycle '(0 41))))
+  (defun ff (time)
+    (let ((note (next notes)))
+      (p time (spawn-circle
+               note 1
+               :shape 0
+               :pencil 0
+               :fade 1
+               :growth '-
+               :selfcolor (v! 1 0 0)
+               ;;:color (v! 0 0 0)
+               )
+         50 1 1))
+    (aat (+ time 1) #'ff it)))
+
+(defun ff ())
+(ff (quant 4))
+(fg .1)
+
+(let ((rhythms (make-cycle '(2/3 4/3)))
+      (notes   (make-weighting '(60 (0 .1) (53 .1)))))
+  (defun f (time)
+    (let ((rhythm (next rhythms))
+          (note   (next notes)))
+      (p time (spawn-circle note rhythm
+                            ;; :shape 1
+                            ;; :pos (v! (cm:interp note 30 -.5 90 .5) -.5)
+                            :growth (if (= note 60) nil '+)
+                            )
+         30 rhythm 0)
+      (aat (+ time rhythm) #'f it))))
+
+(defun f ())
+(f (tempo-sync #[1 b]))
+(aat (quant 4) #'f it)
+
+(defun fff ())
+(let ((notes   (make-weighting '(60 62 (59 .1))))
+      (rhythms (make-weighting '(4 3 (2 .1))))
+      (amps    (make-weighting '(45 (50 .1)))))
+  (defun fff (time)
+    (let ((rhythm (next rhythms)))
+      (p time (spawn-circle (+ (pick 0 12 12) (next notes))
+                            rhythm
+                            :selfcolor (v! 1 0 0)
+                            :shape 3
+                            :growth (pick '+ '- nil)
+                            :rotation (random 45f0))
+         (next amps) .5 3)
+      (aat (+ time rhythm) #'fff it))))
+
+(fff (tempo-sync #[1 b]))
+(fff (quant 4))
+
+(freverb-toggle 1)
+(freverb-preset 6)
+
+(let* ((i (make-cycle '(i iv v iv) (make-weighting '(3 (2 .1)))))
+ ;;      (b (make-cycle '(65 68 70) 10))
+       (c (make-cycle (pval (make-chord-fixed 75 4 (pc-diatonic 0 '- (next i))))
+                      (make-weighting '(1 (2 .4) 3))))
+       (r (make-weighting '(.5 1 (2 .1)))))
+  (defun bb (time)
+    (let ((rhythm (next r))
+          (notes (next c 3)))
+      (p time (spawn-circle (nths notes
+                                  (pick '(0 5) '(0) '(0) '(0 2)))
+                            rhythm
+                            :growth '-
+                            :pos (v! (random 1f0) (random 1f0))
+                            :shape 1
+                            :rotation (if (> (first notes) 70)
+                                          0f0
+                                          (random 90f0))
+;;                            :selfcolor (v3! 0 (random .8))
+                            )
+         35 rhythm 5)
+      (aat (+ time rhythm) #'bb it))))
+
+(defun bb ())
+
+(fp 5 0)
+
+(bb (tempo-sync #[1 b]))
+(bb (quant 4))
+(aat (quant 4) #'bb it)
---
+++ examples/notalent.lisp
@@ -0,0 +1,184 @@
+(in-package :somecepl)
+
+(defvar *notes* nil)
+
+(defclass circle-note ()
+  ((pitch :initarg :pitch :initform nil)
+   (die-at :initarg :die-at :initform -1)
+   (life :initarg :life :initform 0f0)
+   (growth :initarg :growth :initform nil)
+   (pencil :initarg :pencil :initform 1)
+   (pos :initarg :pos :initform (v! 0 0))
+   (color :initarg :color :initform (v! 0 0 0))
+   (selfcolor :initarg :selfcolor :initform (v! 1 1 1))
+   (shape :initarg :shape :initform 0)
+   (fade :initarg :fade :initform 0f0)
+   (rotation :initarg :rotation :initform 0f0)))
+
+(defun mynow ()
+  ;; Just some number that increases over time that we use
+  ;; in a bunch of places
+  (/ (get-internal-real-time)
+     1000f0))
+
+;; --------------------------------------------------
+(defun-g vert ((vert :vec2))
+  (values (v! vert 0 1)
+          (* (+ vert .5) .5)))
+
+(defun-g frag ((uv :vec2) &uniform
+               (resolution :vec2) (time :float) (instance :float))
+  (v! .3 .3 .3 1))
+
+(defpipeline-g pipe ()
+  :vertex (vert :vec2)
+  :fragment (frag :vec2))
+
+(defmacro defcard (() &body body)
+  `(defun-g frag ((uv :vec2) &uniform
+                  (resolution :vec2) (time :float)
+                  (life :float)
+                  (shape :int)
+                  (pencil :int)
+                  (mycolor :vec3)
+                  (rotation :float)
+                  (selfcolor :vec3)
+                  (fade :float)
+                  (pos :vec2) (pitch :int))
+     (let* ((st  (/ (s~ gl-frag-coord :xy)
+                    resolution))
+            (color  (v! 0 0 0))
+            ;; this helps keeping things inside
+            (st (+ .5 (* 1.1912 (- st .5)))))
+       ;; this removes some artifacts on bridges AND
+       ;; keeps the aspect ratio
+       (if (> (y resolution) (x resolution))
+           (progn
+             (multf (y st) (/ (y resolution) (x resolution)))
+             (decf  (y st) (/ (- (* .5 (y resolution))
+                                 (* .5 (x resolution)))
+                              (x resolution))))
+           (progn
+             (multf (x st) (/ (x resolution) (y resolution)))
+             (decf  (x st) (/ (- (* .5 (x resolution))
+                                 (* .5 (y resolution)))
+                              (y resolution)))))
+       ,@body)))
+
+;;--------------------------------------------------
+
+(defparameter *blend*
+  (make-blending-params
+;;   :mode-rgb :func-add
+;;   :mode-alpha :func-add
+   :source-rgb :one
+   :destination-rgb :one
+;;   :source-alpha :one
+;;   :destination-alpha :one
+   ))
+
+
+(defun draw! ()
+  (let ((res (surface-resolution (current-surface)))
+        )
+    (setf (viewport-resolution (current-viewport))
+          res)
+    (as-frame
+      (loop :for x :in *notes* :do
+
+         ;; reduce or increase life if growth is defined
+         (case (slot-value x 'growth)
+           (+ (incf (slot-value x 'life) .05))
+           (- (decf (slot-value x 'life) .05)))
+
+         ;; ...
+         (when (> (slot-value x 'fade) 0)
+           (decf (slot-value x 'fade) .02))
+         
+         ;; kill when life implodes or explodes
+         (when (or (< (slot-value x 'life) 0)
+                   (> (slot-value x 'life) 9))
+           (setf *notes* (remove x *notes*)))
+
+         ;; kill when time expires
+         (when (> (get-universal-time) (slot-value x 'die-at))
+           (setf *notes* (remove x *notes*)))
+         
+         
+         (with-setf (depth-test-function) nil
+           (with-blending *blend*
+             (with-slots (color rotation fade pos
+                                pencil pitch
+                                life shape selfcolor)
+                 x
+               (map-g #'pipe (get-quad-stream-v2)
+                      :resolution res
+                      :time (mynow)
+                      :rotation rotation
+                      :mycolor color
+                      :pos pos
+                      :fade fade
+                      :pencil pencil
+                      :selfcolor selfcolor
+                      :pitch pitch
+                      :shape shape
+                      :life (float life)))))))))
+
+(defun init ()
+  (setf *notes* nil)
+  (push (make-instance 'circle-note
+                       :life 1f0
+                       :pitch 60
+                       :die-at (+ (get-universal-time) 2))
+        *notes*))
+
+(def-simple-main-loop runplay (:on-start #'init)
+  (draw!))
+
+;;--------------------------------------------------
+
+
+(defcard ()
+  (let* (;;(color (v! .3 .1 .2))
+         (color (+ color (stroke (circle-sdf st)
+                                 (* .015 pitch)
+                                 .01)))
+         (color (+ color (* (v! .9 .2 .3) (g-fill (circle-sdf
+                                        st
+                                        )
+                                       .5)))))
+    (v! color)))
+
+(defcard ()
+  (let* ((ost st)
+         (st (pixel-spirit-deck:rotate st (radians rotation)))
+         (ship (cond
+                 ((= shape 0) (circle-sdf (+ pos st)))
+                 ((= shape 3) (* 1.2 life (tri-sdf (+ (v! 0 -.1) st))))
+                 ((= shape 4) (rays-sdf (pixel-spirit-deck:rotate
+                                         st
+                                         (radians (* pitch (sin time))))
+                                        10))
+                 (t (rect-sdf st (v2! (* pitch .01))))))
+         (vshape   (if (= pencil 0)
+                       (g-fill ship (* pitch life .002))
+                       (stroke ship
+                               (* pitch life .01)
+                               .01)))
+         (newcolor (v3! vshape))
+         (newcolor (if (<= fade -1) newcolor (* newcolor fade)))
+         (newcolor (+ newcolor mycolor))
+         (newcolor (* newcolor selfcolor))
+         (newcolor (if (not (= rotation 0))
+                       (if (= shape 1)
+                           (+ newcolor (* .2 (y ost)))
+                           newcolor)
+                       (+ newcolor
+                             (step (v3! (the-book-of-shaders:snoise
+                                         (* (* .09 pitch)  (+ time st))))
+                                   (+ -.9 newcolor)))))
+         ;; (newcolor (+ newcolor (v! (exp (* 100 (tan time) (y st) (x st)))
+         ;;                           0
+         ;;                           0)))
+         )
+    (v! newcolor 0)))
