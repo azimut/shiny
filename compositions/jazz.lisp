4f2c53a0db8d46a0aac066bdadf751ec56370bb2
azimut <azimut.github@protonmail.com>
2018-06-15 02:38:26 -0300

    simple jazz

--- compositions/jazz.lisp
+++ compositions/jazz.lisp
@@ -9,19 +9,6 @@
 (defparameter *jazz-changes* ; key changes
   #(bf3 ef4 bf3 bf ef4 ef bf3 bf f4 ef bf3 bf))
 
-(defun play-midi-note (time pitch velocity dur c)
-  (when (and
-         (not (equal pitch "r"))
-         (> pitch 0)
-         (> velocity 0)
-         (> dur 0))
-    (progn
-      (at time #'fluidsynth:noteon
-          *synth* c pitch velocity)
-      (at (+ time #[dur b]) #'fluidsynth:noteoff
-          *synth* c pitch))))
-
-
 #|
 (defun jazz-high-hat (time &optional notes)
   (when (null notes)
@@ -32,36 +19,40 @@
                  't)))
   (play-midi-note time (first notes) (round (cosr 35 5 1/2)) .33 1)
   (aat (+ time #[1 b]) #'jazz-high-hat it (rest notes)))
-|#
+
 (defun jazz-high-hat ())
 (defun jazz-high-hat (time)
-  (p time 42 (rcosr 25 3 1/2) 1/3 0)
+  (p time 42 (rcosr 25 3 1/2) 1/3 1)
   (aat (+ time 2) #'jazz-high-hat it))
-
 (jazz-high-hat (quant 4))
+|#
+
+
+(fp 1 104)
+(fg 1.0)
+
+(fp 1 49)
 
 (defbeat hithat 1 "x-x-"
-  (p time 42 (rcosr 20 3 1/2) 1/3 0))
-
+  (p time 42 (rcosr 40 3 1/2) 1/3 1))
 (hithat)
 (defun %hithat ())
 
+(fp 0 13)
 (let ((notes   (make-weighting
-                 '((0 :weight .25)
-                   40
-                   35)))
+                '((0 .25)
+                  40
+                  35)))
       (rhythms (make-cycle '(2/3 4/3)))
-      (amps    (make-weighting '(21 (30 :weight .1)))))
+      (amps    (make-weighting '(51 (60 .1)))))
   (defun jazz-drums (time)   
     (let ((rhythm (next rhythms)))
-      (p
-       time (next notes) (next amps) rhythm 0)
+      (p time (next notes) (next amps) rhythm 0)
       (aat (+ time rhythm) #'jazz-drums
            it))))
 
 (defun jazz-drums ())
 (jazz-drums (quant 4))
-(hithat)
 
 ;; wt
 ;; is weight of resting relative to playing
@@ -72,36 +63,38 @@
 (defun or12r (wt)
   (make-weighting
    (list
-    (list
-     (make-weighting `(51 (0 :weight ,wt)) 1)
-     :weight 1.5)
-    (make-weighting `(59 (0 :weight ,wt)) 1))
+    (list (make-weighting `(51 (0 ,wt)) 1) 1.5)
+    (make-weighting `(59 (0 ,wt)) 1))
    2))
 
-(defun lamps (amps)
-  (mapcar
-   #'round
-   (mapcar
-    (lambda (x) (cm:interp x 0.0 30 1.0 70))
-    (cm:amplitude amps))))
+;; (defun lamps (amps)
+;;   (mapcar
+;;    #'round
+;;    (mapcar
+;;     (lambda (x) (cm:interp x 0.0 30 1.0 80))
+;;     (cm:amplitude amps))))
 
 ; Triplet 8th: 1  2  3    4  5  6    7  8  9   10 11 12
 ; Cymbals:     1  -  x    1  -  1    1  x  x    1  x  1 
-(defun jazz-cymbals (time rhythm &optional notes amps)
-  (setp notes (next (new cycle
-                      :of (list
-                           51 0 (or12r 5)
-                           51 0 51
-                           51 (or12r 7) (or12r 7)
-                           51 (or12r 3) 51))
-                    't))
-  (when (null amps)
-    (setf amps (lamps '(:mf :mp :fff :f
-                        :mp :ffff :mf :mp
-                        :fff :f :mp :ffff))))
-  (p time (first notes) (+ -20 (first amps)) rhythm 0)
-  (aat (+ time rhythm) #'jazz-cymbals
-       it rhythm (rest notes) (rest amps)))
+(fp 4 9)
+
+(fg 2.0)
+
+(let ((notes (make-cycle
+              (list
+               51 0 (or12r 5)
+               51 0 51
+               51 (or12r 7) (or12r 7)
+               51 (or12r 3) 51)))
+      (amps (make-cycle '(:mf :mp :fff :f
+                          :mp :ffff :mf :mp
+                          :fff :f :mp :ffff))))
+  (defun jazz-cymbals (time rhythm)
+    (p time (next notes)
+       (round (* 60 (cm:amplitude (next amps))))
+       rhythm 4)
+    (aat (+ time rhythm) #'jazz-cymbals
+         it rhythm)))
 
 (jazz-cymbals (quant 4) (cm:rhythm 't8 60))
 (defun jazz-cymbals ())
@@ -109,44 +102,34 @@
 ;;;
 ;; <3
 ;;;
+(fp 11 20)
+(let ((notes (make-weighting
+              (list
+               (list (make-heap *jazz-scale*
+                                (make-weighting '(1 2 3 4)))
+                     :weight (make-weighting '(1.5 1.65)))
+               0)))
+      (amps (make-weighting (list (make-cycle '(.5 .4 .7))
+                                  (make-cycle '(.6 .5 .8)))))
+      (rhythms (make-weighting (list (list (make-cycle '(2/3 1/3) 12) .4)
+                                     (list (make-cycle '(1/3) 8) .6)))))
+  (defun jazz-piano (time &optional root)
+    (and (odds .4)
+         (setf root (pickl *jazz-changes*)))
+    (let ((rhythm (next rhythms)))
+      (p
+       time
+       (cm:keynum (transpose (next notes) root))
+       (round (* 50 (next amps)))
+       rhythm
+       11)
+      (aat (+ time rhythm)
+           #'jazz-piano
+           it
+           root))))
 
-(defun jazz-piano (time &optional notes rhythms root)
-  (when (null notes)
-    (setf root (random-elt *jazz-changes*))
-    (setf notes (next
-                 (make-weighting
-                  (list
-                   (list (make-heap *jazz-scale* (make-weighting '(1 2 3 4)))
-                         :weight (make-weighting '(1.5 1.65)))
-                   0))
-                 't)))
-  (when (null rhythms)
-    (if (null (odds .65))
-        (setf rhythms (repeat 8 '(2/3 1/3)))
-        (setf rhythms (repeat 8 '(1/3)))
-;;        (setf rhythms (repeat 8 '(1)))
-;;        (setf rhythms (cm:rhythm (repeat 8 '(t8)) 60))
-;;        (setf rhythms (cm:rhythm '(t4 t8 t4 t8 t4 t8 t4 t8) 60))
-        ))  
-  (let ((rhythm (car rhythms)))
-    (p
-     time
-     (cm:keynum (cm:transpose (car notes) root))
-     (round (cosr 50 3 3/4))
-     rhythm
-     11)
-    (aat (+ time rhythm)
-
-         #'jazz-piano
-         it
-         (cdr notes)
-         (cdr rhythms)
-         root)))
-
-(fp 11 62)
-
-(jazz-high-hat (tempo-sync #[1 b]))
-(jazz-cymbals  (tempo-sync #[1 b]) (cm:rhythm 't8 60))
+(fp 11 99)
+(fg 4.0)
 (jazz-piano    (quant 4))
 (defun jazz-piano ())
 
@@ -156,3 +139,54 @@
 (jazz-piano    (funcall *metro* (funcall *metro* 'get-beat 4)) )
 
 (flush-pending)
+
+
+(let* ((tonics (make-weighting (nths *jazz-scale* '(0 2 4 6 7))))
+       (colors (make-weighting (nths *jazz-scale* '(1 3 5 6 8))))
+       (amps   (make-cycle '(.5 .4 1.0 .9 .4 .9 .5 .4 1.0 .9 .5 .9)))
+       (durs   (make-cycle '(2/3 1/3 1/3)))
+       ;; beat map. t is tonic, c is color, r is rest
+       (bmap   (make-cycle (list
+                            ;; 5 possible patters for triplets 1-4
+                            (make-weighting (list (rancyc '(:t r r :c) 1.0)
+                                                  (rancyc '(:t r r r) .25)
+                                                  (rancyc '(:t r :t :c) .22)
+                                                  (rancyc '(:t :c :t :c) .065)
+                                                  (rancyc '(:t :c :t r) .014))
+                                            1)
+                            ;; 5 possible patterns for 5-7
+                            (make-weighting (list (rancyc '(r r :t) 1.0)
+                                                  (rancyc '(r r r) .25)
+                                                  (rancyc '(r :c :t) .22)
+                                                  (rancyc '(:t :c :t) .038)
+                                                  (rancyc '(:t :c r) .007))
+                                            1)
+                            ;; 5 possible patterns for 8-10
+                            (make-weighting (list (rancyc '(r r :c) 1.0)
+                                                  (rancyc '(r :t :c) .415)
+                                                  (rancyc '(r r r) .25)
+                                                  (rancyc '(:c :t :c) .11)
+                                                  (rancyc '(:c :t r) .018))
+                                            1)
+                            ;; 2 possible values for 11
+                            (make-weighting '((r :weight 1) (:t :weight .25)) 1)
+                            ;; 2 possible values for 12
+                            (make-weighting '((r :weight 1) (:c :weight .25)) 1)))))
+  (defun jazz-bass (time)
+    (let ((x (next bmap))
+          (d (next durs))
+          (a (next amps))
+          (k nil))
+      (case x
+        (:t (setf k (next tonics)))
+        (:c (setf k (next colors))))
+      (when (numberp k)
+        (p time (transpose k 60)
+           (round (* 30 a))           
+           d 10))
+      (aat (+ time 1/3) #'jazz-bass it))))
+
+(fp 10 99)
+
+(jazz-bass (quant 4))
+(defun jazz-bass ())
