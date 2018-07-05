9e0caebf61b7421ac15d5327983736e45e97aba7
azimut <azimut.github@protonmail.com>
2018-07-05 02:41:34 -0300

    add cv package

---
+++ cv/package.lisp
@@ -0,0 +1,36 @@
+(uiop:define-package somecepl
+  (:shadowing-import-from #:cm #:between) ;; between is in tmp-func but undef
+  (:use #:cl 
+        #:arrow-macros
+        #:cl-ppcre
+        #:sc)
+  (:import-from #:cm
+                #:eop?
+                #:rhythm
+                ;; modify a list, like a chord
+                #:invert
+                #:transpose
+                #:shuffle
+                ;; lazy evaluation, can be next'd
+                #:pval
+                ;; random boolean
+                #:odds
+                ;; random "single element" picker, still you can use lists of lists
+                #:pick
+                #:pickl
+                ;; random distribution helper
+                #:ran
+                #:drunk ;; brownian noise
+                #:between
+                ;; patterns
+                #:next
+                #:new
+                #:weighting
+                #:markov ;; higher order explicit transition, than nesting patterns
+                #:cycle
+                #:palindrome
+                #:heap))
+
+;; define var, provided originally by incudine
+(in-package :somecepl)
+(defvar *sample-rate* 1d0)
---
+++ somecepl-collider-cv.asd
@@ -0,0 +1,31 @@
+(asdf:defsystem "somecepl-collider-cv"
+  :description "Describe somecepl here"
+  :author "Your Name <your.name@example.com>"
+  :license "Specify license here"
+  :serial t
+  :depends-on (
+               ;; ???
+               #:swank
+               ;; nudruz
+               #:screamer
+               ;; collider
+               #:sc
+               ;; midi-constrol
+;;               #:local-time
+               #:cl-alsaseq
+               ;; fluid
+               #:fluidsynth
+               ;; misc
+               #:arrow-macros
+               #:cm
+               ;; needed on extempore.lisp
+               #:cl-ppcre
+               ;; CV
+               #:common-cv
+               )
+  :components ((:file "cv/package")
+               (:file "musicutils")
+               (:file "nudruz")
+               (:file "extempore")
+               (:file "overtone")
+               (:file "drums")))
