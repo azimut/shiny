6be975d27ac008005ca5adf7dc268b69315ff665
azimut <azimut.github@protonmail.com>
2018-08-10 17:50:20 -0300

    stable bbplay and instruments

--- incudine.lisp
+++ incudine.lisp
@@ -2,22 +2,33 @@
 
 ;; Needs (rt-start) first.
 
+;; (dsp! bplay
+;;     ((buf buffer) rate start-pos
+;;      (loop-p boolean) attenuation rms (cfreq fixnum))
+;;   (:defaults 0d0 -1 0 nil .00001 0 10)
+;;   (with-samples
+;;       ((in (incudine.vug:buffer-play
+;;             buf rate start-pos loop-p #'stop))
+;;        (inn (* in attenuation))
+;;       ;; (inn (incudine.vug:downsamp cfreq inn))
+;;        )
+;;     (out inn inn)))
+
 (dsp! bplay
     ((buf buffer) rate start-pos
      (loop-p boolean) attenuation rms (cfreq fixnum))
   (:defaults 0d0 1 0 nil .00001 0 10)
   (with-samples
       ((in (incudine.vug:buffer-play
-            buf rate start-pos loop-p #'stop))
+            buf rate start-pos loop-p #'incudine:free))
        (inn (* in attenuation))
        (inn (+ inn
+;;               (incudine.vug:buzz 2 .1 2)
              ;; (incudine.vug:downsamp 10 inn)
              ;; (incudine.vug:lpf inn 100 1)
              ;; (incudine.vug:hpf inn 200 10)
              ;; (incudine.vug:butter-hp inn 1000)
-;;             (incudine.vug:downsamp cfreq inn)
-             ))
-       
+               ))       
        )
     (out inn inn)))
 
@@ -32,11 +43,26 @@
 ;;  - right now it uses (at) to stop (word-play),
 ;;    might be buffer-play can support other thing
 ;;    ... I could have used (buffer-read) and (phasor-loop)
+;;    ... on the other hand it makes easy to play backwards
 ;;  - Make it quickload-able
 ;;  - Add slice support. With start and stop as relative values
 
 (defvar *buffers* (make-hash-table :test #'equal))
 (defvar *sample-words* (make-hash-table :test #'equal))
+
+(defun corrupt-node
+    (time id &key (center 1f0) (radius .1) (period 1) (life 3))
+  (declare (sample time) (integer id))
+  "sends random rate changes to the node ID
+   LIFE is needed to avoid early check of the node"
+  (let ((alive (node-id (node id))))
+    (when (or alive (> life 0))
+      (set-control
+       id
+       :rate (funcall (pick '+ '+ '+ '+ '+ '+ '-)
+                      (cosr center radius 10)))
+      (aat (+ time #[period b])
+           #'corrupt-node it id :life (- life 1)))))
 
 (defun pitch-to-ratio (p)
   (declare (integer p))
@@ -66,31 +92,40 @@
 ;;--------------------------------------------------
 ;; Single buffer play
 
-(defun bbplay (buf &key (rate 1) (rpitch 0 rpitch-p)
+(defun bbplay (buf &key (rate 1 rate-p) (rpitch 0 rpitch-p)
                      (beat-length 4 beat-length-p)
+                     (start-pos 0)
                      (loop-p nil) (attenuation 2) (id 2 id-p))
   (declare (buffer buf) (integer rpitch id) (boolean loop-p))
   "plays the provided buffer either by
    RATE plays the buffer to play at rate
-   RPITCH bends the whole buffer rate to play to the new pitch
+   RPITCH bends the whole buffer rate to play to the new pitch offset
    BEAT-LENGTH stretch the whole buffer to play for N beats"
-  (if rpitch-p
-      (setf rate (pitch-to-ratio rpitch)))
-  (if beat-length-p
-      ;; need to calculate the total duration in sec
-      ;; to provide it to (beat-to-ratio
-      (let* ((sample-rate (buffer-sample-rate buf))
-             (size (buffer-size buf))
-             (dur (/ size sample-rate)))
-        (setf rate (beat-to-ratio beat-length
-                                  sample-rate
-                                  dur))))
-  (if id-p
-      (bplay buf rate 0 loop-p
-          :attenuation attenuation
-          :id id)
-      (bplay buf rate 0 loop-p
-          :attenuation attenuation)))
+  ;; big check so we can send nil without crashing
+  (when (or (and rate-p rate)
+            (and beat-length-p beat-length)
+            (and rpitch-p rpitch))
+    (let ((sample-rate (buffer-sample-rate buf))
+          (size   (buffer-size buf))
+          (frames (buffer-frames buf)))
+      (when rpitch-p
+        (setf rate (pitch-to-ratio rpitch)))
+      (when beat-length-p
+        ;; need to calculate the total duration in sec
+        ;; to provide it to (beat-to-ratio
+        (let ((dur (/ size sample-rate)))
+          (setf rate (beat-to-ratio beat-length
+                                    sample-rate
+                                    dur))))
+      ;; fix to work with buffer-play vug
+      (when (< rate 0)
+        (setf start-pos (- frames (/ frames 20))))
+      (if id-p
+          (bplay buf rate start-pos loop-p
+                 :attenuation attenuation
+                 :id id)
+          (bplay buf rate start-pos loop-p
+                 :attenuation attenuation)))))
 
 ;;------------------------------------------------------
 ;; Slice of a single buffer, dedicated to words or phrases
@@ -113,7 +148,11 @@
 (defun word-play (phrase
                   &key (loop-p nil) (attenuation 1f0)
                     (rate 1) (rpitch 1 rpitch-p)
-                    (beat-length 1 beat-length-p) (id 2))
+                    (beat-length 1 beat-length-p) (id 2)
+                    corrupt
+                    (corrupt-center 1f0)
+                    (corrupt-radius .1)
+                    (corrupt-period 1))
   (declare (integer rpitch id) (string phrase) (boolean loop-p))
   "bplay wrapper to play the phrase provided
   (word-play \"time\")
@@ -139,9 +178,15 @@
       (bplay buf rate start-pos loop-p
              :attenuation attenuation
              :id id)
+      (when corrupt
+        (corrupt-node (now)
+                      id
+                      :center corrupt-center
+                      :radius corrupt-radius
+                      :period corrupt-period))
       ;; kill voice
-      (let ((dur (* (/ rate) dur)))
-        (at (+ (now) #[dur b]) #'free (node id))))))
+      (let ((dur (* (/ (abs rate)) dur)))
+        (at (+ (now) #[dur b]) #'incudine:free (node id))))))
 
 
 ;;--------------------------------------------------
@@ -157,7 +202,7 @@
   ((name   :initarg :name)
    (dir    :initarg :dir)
    (lnotes :initarg :lnotes :initform '())
-   (keys   :initarg :keys)))
+   (keys   :initarg :keys   :accessor keys)))
 
 (defun make-instrument (name directory)
   (declare (string directory) (symbol name))
@@ -167,25 +212,33 @@
                        :dir directory
                        :keys (make-array 127))))
 
+(defun list-instruments ()
+  (alexandria:hash-table-keys *instruments*))
+
 (define-vug buffer-loop-play
     ((buffer buffer) rate start-pos loopstart loopend)
   (with-samples
       ((frame (phasor-loop rate start-pos loopstart loopend)))
     (buffer-read buffer frame :interpolation :cubic)))
 
-(dsp! play-lsample-f ((buf buffer) dur amp loopstart loopend rate)
+(dsp! play-lsample-f
+    ((buf buffer) dur amp loopstart loopend rate)
   (:defaults nil 1 1 0 0 1)
-  (stereo (* amp
-             (envelope (make-envelope '(0 1 1 0) '(0 .9 .1))
-                       1 dur #'incudine:stop)
-             (buffer-loop-play buf rate 0 loopstart loopend))))
+  (with-samples ((in (buffer-loop-play buf rate 0 loopstart loopend))
+                 (in (+ (* .6 in)
+                        (incudine.vug:resonr in 400 .9)
+                        )))
+    (stereo (* amp
+               (envelope (make-envelope '(0 1 1 0) '(0 .9 .1))
+                         1 dur #'incudine:free)
+               in))))
 
 (defclass key ()
-  ((filename :initarg :filename)
-   (keynum :initarg :keynum)
-   (startpos :initarg :startpos)
+  ((filename  :initarg :filename)
+   (keynum    :initarg :keynum)
+   (startpos  :initarg :startpos)
    (loopstart :initarg :loopstart)
-   (loopend :initarg :loopend)))
+   (loopend   :initarg :loopend)))
 
 (defun push-note (name keynum filename
                   &optional (startpos 0) loopstart loopend)
@@ -207,96 +260,9 @@
                                :loopstart loopstart
                                :loopend loopend)))))))
 
-(defun play-instrument (name keynum &optional (dur 1) (amp 1) (rate 1))
+(defun play-instrument (name keynum
+                        &optional (dur 1) (amp 1) (rate 1))
   (with-slots (keys) (gethash name *instruments*)
     (with-slots (filename loopend loopstart) (aref keys keynum)
       (let ((buf (gethash filename *buffers*)))
         (play-lsample-f buf dur amp loopstart loopend rate)))))
-
-;;::::::::::::::::::::::::::::::::::::::::::::::::::
-;; Game-Music-Emu - cl-gme - helpers
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; This is a way to load and play around with gme
-;; Another approach possible might be load the whole file as many times
-;; as voices are on the file and then use different helpers to play them.
-;; Might be like that the transitions between adding voices and removing them
-;; would be more seemles...dunno.
-
-;; TODO: shorted hash keys
-
-(ql:quickload :cl-gme/incudine)
-
-(defparameter *loading* (make-hash-table :test #'equal))
-(defparameter *playing* (make-hash-table :test #'equal))
-
-(defun gmeclean ()
-  "frees global hashes of buffers and recreates them"
-  (loop :for key :in (alexandria:hash-table-keys *playing*)
-     :do (incudine:free (gethash key *playing*)))
-  (setf *playing* (make-hash-table :test #'equal))
-  (loop :for key :in (alexandria:hash-table-keys *loading*)
-     :do (incudine:free (gethash key *loading*)))
-  (setf *loading* (make-hash-table :test #'equal)))
-
-(defun gmebuffer (filename &key (len 1) (track-number 0)
-                             (rate 44100) (offset 0)
-                             (voices '()))
-  "returns a incudine buffer with the gme FILENAME loaded into it"
-  (let* ((frames (* len    10 4410)) ;; need to be multiple of 4410
-         (offset (* offset 10 4410)) ;; it's hardcoded on the .c util...
-         (buf    (make-buffer (/ frames 2)
-                              :channels 2
-                              :sample-rate rate)))
-    (cl-gme:with-track
-        (gmefile filename track-number :rate rate :voices voices)
-      (cl-gme::gmefile-to-buffer (buffer-data buf)
-                                 (cffi:mem-ref gmefile :pointer)
-                                 frames
-                                 offset))
-    buf))
-
-(defun gmeplay (filename node track-number
-                &key (attenuation .00001) (rate 1f0) (start-pos 0)
-                  (fade-curve 3) (fade-time 0f0)
-                  (length 1) (offset 0) (voices '())
-                  (load-only nil))
-  "plays provided FILENAME TRACK-NUMBER in NODE id,
-   takes care of loading the file and swapping buffers for live coding
-
-   LOAD-ONLY just put the buffer in the global hash, do not play it
-
-   (gmeplay \"/home/sendai/Downloads/sf2/ff3.nsf\" 2 15
-         :attenuation .00002
-         :length 20
-         :offset 10
-         :voices '(2)
-         :rate 1
-         :fade-time 1
-         :fade-curve 2)"
-  (declare (integer node track-number length offset))
-  (let ((alive   (node-id (node node)))
-        (hashkey (concatenate 'string filename (write-to-string node))))
-    (setf (gethash hashkey *loading*)
-          (gmebuffer filename
-                     :track-number track-number
-                     :len length
-                     :voices voices
-                     :offset offset))
-    (unless load-only
-      (if alive
-          (set-controls
-           node
-           :buf (gethash hashkey *loading*)
-           :rate rate
-           :fade-curve fade-curve
-           :fade-time fade-time
-           :attenuation attenuation)
-          (bplay (gethash hashkey *loading*) rate 0 t
-                 :id node
-                 :fade-curve fade-curve
-                 :fade-time fade-time
-                 :attenuation attenuation)))
-    (rotatef (gethash hashkey *loading*)
-             (gethash hashkey *playing*))
-    (incudine:free (gethash hashkey *loading*))))
