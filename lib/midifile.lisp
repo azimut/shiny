857da5d4cca99a3bb6c90945dd0651221eb20aa6
azimut <azimut.github@protonmail.com>
2018-09-06 10:12:30 -0300

    midifile: add notes-list, check correct event

--- lib/midifile.lisp
+++ lib/midifile.lisp
@@ -1,5 +1,9 @@
 (in-package :shiny)
 
+;; TODO:
+;; - It would be nice parse and return chords with time
+;; - Identify rests as a new event?
+
 (defvar *qnotes* nil)
 
 (defun go-track (to-track mf)
@@ -12,29 +16,34 @@
        :do (midifile:next-track mf))))
 
 (defun get-notes (filename &optional (track-number 0))
-  "just return the midi notes"
+  "just return the midi notes, MOSTLY useful with bars without chords
+   > (get-notes *mf*)
+   (60 64 69 34 59 30 49 59 29)"
   (declare (string filename))
   (midifile:with-open-midifile (mf filename)
     (go-track track-number mf)
     (loop
        :for ev = (midifile:read-event mf)
        :while ev
-       :when (and (< ev 240) ;; #xf0
+       :when (and (= ev 144) ;;(< ev 240) ;; #xf0
                   (> (midifile:message-data2 mf) 0))
        :collect (midifile:message-data1 mf))))
 
 (defun push-midi-note (note time-seconds)
-  "helper for (get-notes-and-velocity)"
+  "helper for (get-notes-duration)"
   (let ((current (gethash note *qnotes*)))
     (if current
         (progn (setf (gethash note *qnotes*) nil)
-               (list note (- time-seconds (cadr current))))
+               (list note (coerce (- time-seconds (cadr current))
+                                  'single-float)))
         (progn (setf (gethash note *qnotes*)
                      (list note time-seconds))
                nil))))
 
-(defun get-notes-and-velocity (filename &optional (track-number 0))
-  "get notes and velicity as pairs, ONLY useful for bars with only one note"
+(defun get-notes-duration (filename &optional (track-number 0))
+  "get notes and duration as pairs, ONLY useful for bars with only one note
+   > (get-notes-duration *mf*)
+   ((50 .2) (60 .2) (90 1.3))"
   (declare (string filename))
   (setf *qnotes* (make-hash-table))
   (midifile:with-open-midifile (mf filename)
@@ -44,6 +53,35 @@
      (loop
         :for ev = (midifile:read-event mf)
         :while ev
-        :when (< ev 240) ;; #xf0
+        :when (= ev 144) ;;(< ev 240) ;; #xf0
         :collect (push-midi-note (midifile:message-data1 mf)
                                  (midifile:event-seconds mf))))))
+
+(defun get-notes-list (filename &optional (track-number 0))
+  "get notes grouped by time they were triggered
+   > (get-notes-list *mf*)
+   ((60 62 65) (60 62 69) (79)"
+  (declare (string filename))
+  (midifile:with-open-midifile (mf filename)
+    (go-track track-number mf)
+    (remove-if
+     #'null
+     (loop
+        :for ev = (midifile:read-event mf)
+        :while ev
+        :with queue = '()
+        :when (and (= 144 ev) (> (midifile:message-data2 mf) 0)) ;; (< ev 240) ;; #xf0
+        :collect
+        (let* ((time (midifile:event-seconds mf))
+               (now  (list (midifile:message-data1 mf)
+                           time)))
+          (cond ((not queue)
+                 (push now queue) nil)
+                ((and queue (= time (cadar queue)))
+                 (push now queue) nil)
+                ;; Time to return the queue
+                ((and queue (not (= time (cadar queue))))
+                 (prog1 (mapcar #'car queue)
+                   (setf queue nil)
+                   (push now queue))))))))))
+
