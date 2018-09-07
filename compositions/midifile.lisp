b25d22453750ca91234fdf94f7ad836546b4e68f
azimut <azimut.github@protonmail.com>
2018-09-07 04:36:57 -0300

    hurray!

--- compositions/midifile.lisp
+++ compositions/midifile.lisp
@@ -1,30 +1,48 @@
 (in-package :shiny)
+
 (defparameter *mf*
   "/home/sendai/Downloads/Radiohead_-_Daydreaming.mid")
 
-(defparameter *notes* (get-notes *mf*))
+(defparameter *notes*  (subseq (get-notes *mf*) 3 30))
 
-(defparameter *notes* (subseq (get-notes-and-velocity *mf*) 30 60))
+(defparameter *mnotes* (cm:markov-analyze (mapcar #'car *notes*) :order 2))
+(defparameter *notes*  (subseq (get-notes-duration *mf*) 30 60))
+
+
+
+(defparameter *notes*  (subseq (get-notes *mf*) 1000 1160))
 (defparameter *mnotes*
-    (cm:markov-analyze (mapcar #'car *notes*) :order 3))
+  (cm:markov-analyze (get-notes-list *mf* 1) :order 2))
 
 (defun f ())
-(let ((notes (make-cycle *notes*))
-      ;;(notes (make-cycle (mapcar #'car *notes*)))
-      ;; (rhythm (make-cycle
-      ;;          (mapcar (lambda (x) (coerce x 'single-float))
-      ;;                  (mapcar #'cadr *notes*))))
-      )
-  (defun f (time)
+(defparameter *metre* (make-metre '(4) 1))
+(defparameter *notes*  (subseq (get-notes *mf*) 3 30))
+(let ((notes (make-cycle *notes*)))
+  (defun f (time &optional (beat 0))
     (let ((n (next notes))
-          ;;(n (next *mnotes*))
-;;          (r (next rhythm))
+          (ps (odds .2))
           (r .5))
-      (p time n 60 r 0)
-;;      (p time )
-;;      (play-newfm (+ 12 n) 2)
-      (aat (+ time #[r b]) #'f it))))
+      (p time (+ -12 n) (rcosr 60 5 5) (+ 1 r) 0)
+      (when (and (not ps) (= 1 (funcall *metre* beat)))
+        (p time (cm:transpose (print (next *mnotes*)) +24) 50 2 1))
+      (and ps (play-sand (+ 12 n) 2 :amp (rcosr 100 50 5)))
+      (aat (+ time #[r b]) #'f it (+ .5 beat)))))
 
 (f (now))
+(fp 0 0)
+(fp 0 33)
+(fp 1 69)
+(fg 1f0)
 
-(play-pluck 90 1)
+(defparameter *notes*  (subseq (get-notes *mf*) 1020 1100))
+(defparameter *metre* (make-metre '(8) 1))
+(let ((notes (make-cycle *notes*)))
+  (defun f (time &optional (beat 0))
+    (let ((n (next notes))
+          (ps (odds .2))
+          (r .3))
+      (p time (+ -12 n) (rcosr 60 5 5) (+ 1 r) 0)
+      (when (and (not ps) (= 1 (funcall *metre* beat)))
+        (p time (cm:transpose (print (next *mnotes*)) +24) 40 4 1))
+      (and ps (play-sand (+ 12 n) 1 :amp (rcosr 350 50 5)))
+      (aat (+ time #[r b]) #'f it (+ .5 beat)))))
