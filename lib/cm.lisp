ec5c178a73ffc7616789e724ea74ec9d90cc0e49
azimut <azimut.github@protonmail.com>
2018-09-09 00:25:36 -0300

    add cm rotation, accumulation

--- lib/cm.lisp
+++ lib/cm.lisp
@@ -15,7 +15,25 @@
       (new heap :of elements)))
 
 (defun make-line (elements)
+  "Useful for fade in/out, might be with (iota)"
   (new cm:line :of elements))
+
+(defun make-rotation (elements &optional (for-elements 1 for-elements-p))
+  "A constantly rotating pattern.
+   > (next (make-rotation '(1 2 3 4)) 30)
+   (1 2 3 4 2 3 4 1 3 4 1 2 4 1 2 3 1 2 3 4 2 3 4 1 3 4 1 2 4 1)"
+  (if for-elements-p
+      (new cm:rotation :of elements :for for-elements)
+      (new cm:rotation :of elements)))
+
+(defun make-accumulation (elements &optional (for-elements 1 for-elements-p))
+  "For each item, generates all items up to and including the current item.
+   The process starts over when all the items have been accumulated.
+   > (cm:next (cm:new cm:accumulation :of '(1 2 3 4)) 10)
+   (1 1 2 1 2 3 1 2 3 4)"
+  (if for-elements-p
+      (new cm:accumulation :of elements :for for-elements)
+      (new cm:accumulation :of elements)))
 
 (defun make-weighting
     (elements &optional (for-elements 1 for-elements-p))
