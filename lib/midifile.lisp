256ab582362802c0fced8b19bdbb9c315a8ae1f9
azimut <azimut.github@protonmail.com>
2018-12-26 22:22:24 -0300

    midifile: cosmetic

--- lib/midifile.lisp
+++ lib/midifile.lisp
@@ -1,18 +1,28 @@
 (in-package :shiny)
+;; TODO: go-track loop end check for outer range.
+;;
+;; Reference:
+;; List of midi events in hexadecimal
+;; http://www.onicos.com/staff/iz/formats/midi-event.html
+;;
+;; 144 - 0x90 - Chan 1 Note on
+;; 176 - 0xB0 -	Chan 1 Control mode change
+;; 192 - 0xC0 - Chan 1 Program change
+;; 255 - 0xFF -	Sys real time sys reset
 
 (defvar *qnotes* nil)
 
 (defun go-track (to-track mf)
   "skip ahead to-track"
-  (declare (integer to-track) (midifile:input-stream mf))
-  (let* ((number-of-tracks (midifile:number-of-tracks mf))
+  (declare (unsigned-byte to-track) (midifile:input-stream mf))
+  (let* ((number-of-tracks (1- (midifile:number-of-tracks mf)))
          (current-track    (midifile:current-track mf))
-         (to-track         (max 0 (min to-track (1- number-of-tracks)))))
+         (to-track         (max (min to-track number-of-tracks) 0)))
     (loop :repeat (- to-track current-track)
        :do (midifile:next-track mf))))
 
 (defun get-notes (filename &optional (track-number 0))
-  "just return the midi notes, MOSTLY useful with bars without chords
+  "just return the midi notes, useful with bars without chords
    > (get-notes *mf*)
    (60 64 69 34 59 30 49 59 29)"
   (declare (string filename))
@@ -23,7 +33,7 @@
        :while ev
        :when (and (= ev 144)
                   (= track-number (midifile:current-track mf))
-                  (> (midifile:message-data2 mf) 0))
+                  (plusp (midifile:message-data2 mf)))
        :collect (midifile:message-data1 mf))))
 
 (defun push-midi-note
@@ -35,20 +45,20 @@
     ;; NOTE: some midi files have two hits without release between them,
     ;;       avoiding those for now
     (if current
-        (when (= velocity 0)
-          (setf (gethash note *qnotes*) nil)
+        (when (zerop velocity)
+          (setf (gethash note *qnotes*) NIL)
           (let ((result (list note (- time-seconds (cadr current)))))
             (when start-time
               (alexandria:appendf result (list time-seconds)))
             result))
-        (when (not (= 0 velocity))
+        (when (plusp velocity)
           (setf (gethash note *qnotes*)
                 (list note time-seconds))
-          nil))))
+          NIL))))
 
 (defun get-notes-durations
     (filename &optional (track-number 0) start-time coerce)
-  "get notes and duration as pairs, ONLY useful for bars with only one note
+  "get notes and duration as pairs, useful for bars with only one note
    > (get-notes-duration *mf*)
    ((50 .2) (60 .2) (90 1.3))"
   (declare (string filename))
@@ -61,16 +71,17 @@
         :for ev = (midifile:read-event mf)
         :while ev
         :with previous-time = -1
-        :when (and (= ev 144) (= track-number (midifile:current-track mf)))
+        :when (and (= ev 144)
+                   (= track-number (midifile:current-track mf)))
         :collect
-        (let ((seconds (midifile:event-seconds mf)))
-          (when (not (= previous-time seconds))
-            (setf previous-time seconds))
-          (push-midi-note (midifile:message-data1 mf)
-                          seconds
-                          (midifile:message-data2 mf)
-                          start-time
-                          coerce))))))
+          (let ((seconds (midifile:event-seconds mf)))
+            (when (not (= previous-time seconds))
+              (setf previous-time seconds))
+            (push-midi-note (midifile:message-data1 mf)
+                            seconds
+                            (midifile:message-data2 mf)
+                            start-time
+                            coerce))))))
 
 (defun get-notes-durations-chords
     (filename &optional (track-number 0) start-time coerce)
@@ -80,25 +91,27 @@
    #'null
    (loop
       :for (note duration time)
-      :in  (sort (get-notes-durations filename track-number t coerce)
+      :in  (sort (get-notes-durations filename track-number T coerce)
                  #'< :key #'caddr)
       :with queue
       :with last-time
       :collect
       (cond ((not queue)
-             (prog1 nil
+             (prog1 NIL
                (setf last-time time)
                (setf queue `(,note ,duration))))
             ((and queue (= last-time time))
-             (prog1 nil
-               (let ((notes     (append (alexandria:ensure-list (car  queue)) (list note)))
-                     (durations (append (alexandria:ensure-list (cadr queue)) (list duration))))
+             (prog1 NIL
+               (let ((notes     (append (alexandria:ensure-list (car  queue))
+                                        (list note)))
+                     (durations (append (alexandria:ensure-list (cadr queue))
+                                        (list duration))))
                  (setf queue (list notes durations)))))
             ((and queue (not (= last-time time)))
              (let ((result queue))
                (when start-time
                  (alexandria:appendf result (list last-time)))
-               (setf last-time  time)
+               (setf last-time time)
                (setf queue `(,note ,duration))
                result))))))
 
@@ -119,18 +132,18 @@
                    (= track-number (midifile:current-track mf))
                    (> (midifile:message-data2 mf) 0))
         :collect
-        (let* ((time (midifile:event-seconds mf))
-               (now  (list (midifile:message-data1 mf)
-                           time)))
-          (cond ((not queue)
-                 (push now queue) nil)
-                ((and queue (= time (cadar queue)))
-                 (push now queue) nil)
-                ;; Time to return the queue
-                ((and queue (not (= time (cadar queue))))
-                 (prog1 (mapcar #'car queue)
-                   (setf queue nil)
-                   (push now queue)))))))))
+          (let* ((time (midifile:event-seconds mf))
+                 (now  (list (midifile:message-data1 mf)
+                             time)))
+            (cond ((not queue)
+                   (push now queue) NIL)
+                  ((and queue (= time (cadar queue)))
+                   (push now queue) NIL)
+                  ;; Time to return the queue
+                  ((and queue (not (= time (cadar queue))))
+                   (prog1 (mapcar #'car queue)
+                     (setf queue NIL)
+                     (push now queue)))))))))
 
 (defun get-notes-durations-chords-silences
     (filename &optional (track-number 0) start-time (coerce t))
@@ -142,24 +155,25 @@
       :in  (get-notes-durations-chords filename track-number t coerce)
       :with next-time
       :appending
-      (let ((current (list notes durations)))
-        (when start-time
-          (alexandria:appendf current (list time)))
-        (cond
-          ((not next-time)
-           (setf next-time (+ time (first (sort (alexandria:ensure-list durations) #'>))))
-           (list current))
-          ((and next-time
-                (< next-time time)
-                ;; NOTE: Hacks! basically we skip silences too small product
-                ;;       of the flaky math
-                (> (- time next-time) .02))
-           (let* ((zero-duration (- time next-time))
-                  (zero-start    (- time zero-duration)))
+        (let ((current (list notes durations)))
+          (when start-time
+            (alexandria:appendf current (list time)))
+          (cond
+            ((not next-time)
+             (setf next-time
+                   (+ time (first (sort (alexandria:ensure-list durations) #'>))))
+             (list current))
+            ((and next-time
+                  (< next-time time)
+                  ;; NOTE: Hacks! basically we skip silences too small product
+                  ;;       of the flaky math
+                  (> (- time next-time) .02))
+             (let* ((zero-duration (- time next-time))
+                    (zero-start    (- time zero-duration)))
+               (setf next-time (+ time (first (sort (alexandria:ensure-list durations) #'>))))
+               (if start-time
+                   (list current (list 0 zero-duration zero-start))
+                   (list current (list 0 zero-duration)))))
+            (t
              (setf next-time (+ time (first (sort (alexandria:ensure-list durations) #'>))))
-             (if start-time
-                 (list current (list 0 zero-duration zero-start))
-                 (list current (list 0 zero-duration)))))
-          (t
-           (setf next-time (+ time (first (sort (alexandria:ensure-list durations) #'>))))
-           (list current)))))))
+             (list current)))))))
