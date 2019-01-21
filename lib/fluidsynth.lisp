e7d7350304d963189bb8bf8f7f16d9d4987ddaf6
azimut <azimut.github@protonmail.com>
2019-01-21 01:52:38 -0300

    fluidsynth: formatting and sf2 loader helper

--- lib/fluidsynth.lisp
+++ lib/fluidsynth.lisp
@@ -1,4 +1,4 @@
-(in-package :shiny)
+(in-package #:shiny)
 
 (defvar *settings* (fluidsynth:new-settings
                     `(("synth.polyphony" 128)
@@ -118,7 +118,7 @@
             notes
             offsets
             velocity
-            channel)))  
+            channel)))
 (defmethod pa ((time double-float) (notes list) (velocity integer) (duration list) (channel integer) (offset number) &key pan)
   (let* ((lnotes  (length notes))
          (offsets (loop :for i :from 0 :by offset :collect i :repeat lnotes)))
@@ -202,6 +202,11 @@
   "reset all the things! (programs and synths)"
   (fluidsynth:system-reset *synth*))
 
+(defun off-with-the-notes (&optional (max-channels 32))
+  "turn off all the notes"
+  (dotimes (chan max-channels)
+    (fluidsynth:all-notes-off *synth* chan)))
+
 ;; ---------------------------------------------------
 
 ;; http://midi.teragonaudio.com/tech/midispec/pressure.htm
@@ -209,18 +214,20 @@
   "Set the MIDI channel pressure controllre value.
    a.k.a wibbly-woobly
    0>=pressure>=127"
-  (declare (type integer channel pressure))
+  (declare (type (integer 0 127) pressure))
   (fluidsynth:channel-pressure *synth* channel pressure))
 
 ;; http://midi.teragonaudio.com/tech/midispec.htm
 (defun fsens (channel sens)
   "Set MIDI pitch wheel (aka sensitivity) on a MIDI channel.
    0>=sens>=72"
+  (declare (type (integer 0 72) sens))
   (fluidsynth:pitch-wheel-sens *synth* channel sens))
 
 (defun fpitch (channel bend)
   "Set the MIDI pitch bend controller value on a MIDI channel.
    0>=bend>=16383"
+  (declare (type (integer 0 16383) bend))
   (fluidsynth:pitch-bend *synth* channel bend))
 
 ;; ---------------------------------------------------
@@ -231,12 +238,11 @@
     (1 (fluidsynth:set-chorus-on *synth* 1))
     (t (format nil "set 0 or 1 dummy!"))))
 
-(defun fchorus (&key
-                  (nr        3 nr-set)
-                  (level 2.0d0 level-set)
-                  (speed 0.3d0 speed-set)
-                  (depth 8.0d0 depth-set)
-                  (mode      0 mode-set))
+(defun fchorus (&key (nr        3 nr-set)
+                     (level 2.0d0 level-set)
+                     (speed 0.3d0 speed-set)
+                     (depth 8.0d0 depth-set)
+                     (mode      0 mode-set))
   (assert (and (<= 0 nr 99)
                (<= 0d0 level 10d0)
                (<= 0.29d0 speed 5.0d0)
@@ -256,18 +262,12 @@
     (1 (fluidsynth:set-reverb-on *synth* 1))
     (t (format nil "set 1 to enable or 0 disable"))))
 
-(defun freverb (&key (roomsize 0.2d0 roomsize-set)
-                  (damp 0.0d0 damp-set)
-                  (width 0.5d0 width-set)
-                  (level 0.9d0 level-set))
-  (assert (and (<= 0d0 roomsize 1d0)
-               (<= 0d0 damp 1d0)
-               (<= 0d0 width 100d0)
-               (<= 0d0 level 1d0)))
-  (when (or roomsize-set damp-set width-set level-set)
-    (fluidsynth:set-reverb *synth*
-                           roomsize damp
-                           width level)))
+(defun freverb (&key (roomsize 0.2d0 roomsize-p)
+                     (damp     0.0d0 damp-p)
+                     (width    0.5d0 width-p)
+                     (level    0.9d0 level-p))
+  (when (or roomsize-p damp-p width-p level-p)
+    (fluidsynth:set-reverb *synth* roomsize damp width level)))
 
 ;; fluid_synth.c
 (defun freverb-preset (preset)
@@ -289,36 +289,46 @@
     (fluidsynth:bank-select *synth* channel bank))
   (fluidsynth:program-change *synth* channel program))
 
-(defun fg (gain)
-  "short-hand to set the gain on fluidsynth"
-  (declare (type float gain))
-  (setf (fluidsynth:setting *settings* "synth.gain")
-        gain))
-
-(defun off-with-the-notes (&optional (max-channels 50))
-  "turn off all the notes"
-  (loop :for c :below max-channels
-     :do (fluidsynth:all-notes-off *synth* c)))
-
-(defun all-piano (&optional (sf 0))
-  "set all the midi channels to ..."
-  (dotimes (i 32)
-    (fluidsynth:program-change *synth* i sf)))
+(defun all-piano (&optional (program 0) (max-channels 32))
+  "set all the midi channels to PROGRAM"
+  (dotimes (chan max-channels)
+    (fluidsynth:program-change *synth* chan program)))
 
 (defun try-sounds (time chan sf &optional (beats 8) (bank 0 bank-set))
-  "iterate over soundfonts to test sounds"
+  "helper to iterate over programs on a soundfont to test sounds"
   (print sf)
   (when bank-set
     (fluidsynth:bank-select *synth* chan bank))
   (fluidsynth:program-change *synth* chan (mod sf 100))
   (aat (+ time beats) #'try-sounds it chan (1+ sf) beats))
 
+;;--------------------------------------------------
+
+(defun fg (gain)
+  "short-hand to set the gain on fluidsynth"
+  (declare (type float gain))
+  (setf (fluidsynth:setting *settings* "synth.gain")
+        gain))
+
+;;--------------------------------------------------
+
 (defun fpan (channel pan)
-  (declare (unsigned-byte channel) ((integer 0 127)))
+  (declare (type (integer 0 127) pan))
   (fluidsynth:cc *synth* channel 10 pan))
 
-(defun all-pan (&optional (pan 64) (up-to 10))
-  (dotimes (channel up-to)
+(defun all-pan (&optional (pan 64) (max-channels 32))
+  "Set PAN on all channels"
+  (declare (type (integer 0 127) pan))
+  (dotimes (channel max-channels)
     (fluidsynth:cc *synth* channel 10 pan)))
+
+;;--------------------------------------------------
+
+(defun fload (path)
+  "Load given soundfont into synth"
+  (declare (type string path))
+  (assert (probe-file path))
+  (assert (string= "sf2" (last-elt (cl-ppcre:split #\. path))))
+  (fluidsynth:sfload *synth* path 1))
 
 ;;--------------------------------------------------
