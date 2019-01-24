c55f4bd8b57452dee296567a4ea9fc77ff0008e1
azimut <azimut.github@protonmail.com>
2019-01-24 18:06:25 -0300

    cm: make-graph, make-var now with opt for param

--- lib/cm.lisp
+++ lib/cm.lisp
@@ -8,7 +8,6 @@
 ;; https://ccrma.stanford.edu/courses/220b-winter-2006/cm/doc/dict/patterns-topic.html
 ;;
 ;; TODO:
-;; (make-graph)
 ;; (make-markov)
 ;; (make-thunk)
 ;; (make-rewrite)
@@ -18,7 +17,7 @@
 ;; (make-copier)
 
 (defun make-cycle (elements &optional (for-elements 1 for-elements-p))
-  (when elements ;; do not make a cycle of just nil
+  (when elements ;; do not make a cycle of ONLY nil (?
     (if for-elements-p
         (new cycle :of elements :for for-elements)
         (new cycle :of elements))))
@@ -53,27 +52,24 @@
   ;; try to add :weight keyword if only provided 2 elements
   ;; we push since order doesn't really matter...
   (let ((sane-elements '()))
-    (loop for e in elements do
-         (if (and (listp e) (= 3 (length e)))
-             (push e sane-elements))
-         (if (not (listp e))
-             (push e sane-elements))
-         (if (and (listp e) (= 2 (length e)))
-             (push (list (first e)
-                         :weight
-                         (first (last e)))
-                   sane-elements)))
+    (loop
+      :for e :in elements :do
+         (cond ((and (listp e) (length= 3 e))
+                (push e sane-elements))
+               ((not (listp e))
+                (push e sane-elements))
+               ((and (listp e) (length= 2 e))
+                (push (list (first-elt e) :weight (last-elt e))
+                      sane-elements))))
     (if for-elements-p
-        (new weighting :of sane-elements :for for-elements)
-        (new weighting :of sane-elements))))
+        (new cm:weighting :of sane-elements :for for-elements)
+        (new cm:weighting :of sane-elements))))
 
 ;; FIXME!!
-(defun make-stutter
-    (elements &optional (for 1))
+(defun make-stutter (elements &optional (for 1))
   (cm:new cm:cycle
     :of elements
-    :for
-    (cm:new cm:cycle :of for)))
+    :for (cm:new cm:cycle :of for)))
 
 (defun make-palindrome (elements)
   (cm:new cm:palindrome :of elements :elide t))
@@ -89,35 +85,72 @@
   (declare (markov markov))
   (let ((long-cycle (next markov 30)))
     (loop
-       :for cycle :in long-cycle
-       :with queue
-       :while (not (position cycle queue))
-       :finally (return queue)
-       :do (push cycle queue))))
+      :for cycle :in long-cycle
+      :with queue
+      :while (not (position cycle queue))
+      :finally (return queue)
+      :do (push cycle queue))))
+
+(defun make-graph (elements &optional (for 1 for-p))
+  "The Graph pattern. A graph is a network of nodes, each node contains
+  a 'value' (the item to return from the pattern), a 'link' to the
+  node that should come next , and an identifier (a unique name for
+  the node in the graph). Both the value and the link can be
+  subpatterns . node identifiers default to increasing numbers from 1.
+
+  Example:
+  > (make-graph '((A 2) (B 3) (C 1))))"
+  (let ((pat (loop
+               :for id :from 1
+               :for (elt to-id) :in elements
+               :collect (list elt :id id :to to-id))))
+    (if for-p
+        (cm:new cm:graph :of pat :for for)
+        (cm:new cm:graph :of pat))))
 
 ;;------------------------------------------------------------
 ;; TODO: sub-cycles, like (make-var '(4 2) '(1 2 (2 (3 4))))
 ;; Version that takes 2 lists
-(defgeneric make-var (elements for)
-  (:documentation "Returns a LIST of cycles and/or symbols.
-FOR >1 is useful to stutter ELEMENTS.")
-  (:method ((elements list) (for fixnum))
-    (loop
-       :for element :in elements
-       :collect
-         (if (and (= for 1)
-                  (or (null element) (not (listp element))))
-             element
-             (cm:new cm:cycle :of element :for for))))
-  (:method ((elements list) (for list))
-    (loop
-       :for element :in elements
-       :for f :in for
-       :collect
-         (if (and (= f 1)
-                  (or (null element) (not (listp element))))
-             element
-             (cm:new cm:cycle :of element :for f)))))
+(defun make-var (elements &optional (for NIL for-p))
+  "Returns a LIST of cycles and/or symbols.
+   FOR NIL is usefult to play plain lists of cycles
+   FOR =1 is useful for
+   FOR >1 is useful to stutter ELEMENTS."
+  (declare (type list elements))
+  (if for-p
+      (loop
+        :for element :in elements
+        :for f :in (ensure-list for)
+        :collect
+           (if (and (= f 1) (or (null element) (not (listp element))))
+               element
+               (cm:new cm:cycle :of element :for f)))
+      (loop
+        :for element :in elements
+        :collect
+           (if (or (null element) (not (listp element)))
+               element
+               (cm:new cm:cycle :of element)))))
+
+(defun make-war (elements &optional (for NIL for-p))
+  "Returns a LIST of weighted and/or symbols.
+   FOR >1 is useful to stutter ELEMENTS."
+  (declare (type list elements))
+  (if for-p
+      (loop
+        :for element :in elements
+        :for f :in (ensure-list for)
+        :collect
+           (if (and (= f 1)
+                    (or (null element) (not (listp element))))
+               element
+               (make-weighting element for)))
+      (loop
+        :for element :in elements
+        :collect
+           (if (or (null element) (not (listp element)))
+               element
+               (make-weighting element)))))
 
 ;; Version that takes 1+N arguments
 (defgeneric make-cycles (for &rest rest)
@@ -135,8 +168,8 @@ FOR >1 is useful to stutter ELEMENTS.")
                 rest)))
   (:method ((for list) &rest rest)
     (loop
-       :for f :in for
-       :for r :in rest :collect
+      :for f :in for
+      :for r :in rest :collect
          (if (= 1 f)
              r
              (cm:new cm:cycle
