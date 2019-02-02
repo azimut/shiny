ff178f818e01166214bd14f8259bbfe021fa58c8
azimut <azimut.github@protonmail.com>
2019-02-01 21:06:17 -0300

    cm: add pshuf like pattern, and some from nudruz

--- lib/cm.lisp
+++ lib/cm.lisp
@@ -17,27 +17,32 @@
 ;; (make-copier)
 
 (defun make-cycle (elements &optional (for-elements 1 for-elements-p))
+  "loops over elements in a continuous cycle"
   (when elements ;; do not make a cycle of ONLY nil (?
     (if for-elements-p
-        (new cycle :of elements :for for-elements)
-        (new cycle :of elements))))
+        (cm:new cm:cycle :of elements :for for-elements)
+        (cm:new cm:cycle :of elements))))
+
+(defun make-coin ()
+  "returns a cycle that iterates between NIL and T"
+  (make-cycle '(NIL T)))
 
 (defun make-heap (elements &optional (for-elements 1 for-elements-p))
   (if for-elements-p
-      (new heap :of elements :for for-elements)
-      (new heap :of elements)))
+      (cm:new cm:heap :of elements :for for-elements)
+      (cm:new cm:heap :of elements)))
 
 (defun make-line (elements)
-  "Useful for fade in/out, might be with (iota)"
-  (new cm:line :of elements))
+  "loops over elements in a continuous cycle"
+  (cm:new cm:line :of elements))
 
 (defun make-rotation (elements &optional (for-elements 1 for-elements-p))
   "A constantly rotating pattern.
    > (next (make-rotation '(1 2 3 4)) 30)
    (1 2 3 4 2 3 4 1 3 4 1 2 4 1 2 3 1 2 3 4 2 3 4 1 3 4 1 2 4 1)"
   (if for-elements-p
-      (new cm:rotation :of elements :for for-elements)
-      (new cm:rotation :of elements)))
+      (cm:new cm:rotation :of elements :for for-elements)
+      (cm:new cm:rotation :of elements)))
 
 (defun make-accumulation (elements &optional (for-elements 1 for-elements-p))
   "For each item, generates all items up to and including the current item.
@@ -45,8 +50,8 @@
    > (cm:next (cm:new cm:accumulation :of '(1 2 3 4)) 10)
    (1 1 2 1 2 3 1 2 3 4)"
   (if for-elements-p
-      (new cm:accumulation :of elements :for for-elements)
-      (new cm:accumulation :of elements)))
+      (cm:new cm:accumulation :of elements :for for-elements)
+      (cm:new cm:accumulation :of elements)))
 
 (defun make-weighting (elements &optional (for-elements 1 for-elements-p))
   ;; try to add :weight keyword if only provided 2 elements
@@ -62,8 +67,8 @@
                 (push (list (first-elt e) :weight (last-elt e))
                       sane-elements))))
     (if for-elements-p
-        (new cm:weighting :of sane-elements :for for-elements)
-        (new cm:weighting :of sane-elements))))
+        (cm:new cm:weighting :of sane-elements :for for-elements)
+        (cm:new cm:weighting :of sane-elements))))
 
 ;; FIXME!!
 (defun make-stutter (elements &optional (for 1))
@@ -72,9 +77,11 @@
     :for (cm:new cm:cycle :of for)))
 
 (defun make-palindrome (elements)
+  "generates elements forwards and backwards"
   (cm:new cm:palindrome :of elements :elide t))
 
 (defun make-range (from to &optional (by 1))
+  "iterates numbers in a range"
   (cm:new cm:range :from from :to to :by by))
 
 ;; took from jazz.cm
@@ -101,9 +108,9 @@
   Example:
   > (make-graph '((A 2) (B 3) (C 1))))"
   (let ((pat (loop
-               :for id :from 1
-               :for (elt to-id) :in elements
-               :collect (list elt :id id :to to-id))))
+                :for id :from 1
+                :for (elt to-id) :in elements
+                :collect (list elt :id id :to to-id))))
     (if for-p
         (cm:new cm:graph :of pat :for for)
         (cm:new cm:graph :of pat))))
@@ -119,39 +126,55 @@
   (declare (type list elements))
   (if for-p
       (loop
-        :for element :in elements
-        :for f :in (repeat (length elements)
-                           (ensure-list for))
-        :collect
+         :for element :in elements
+         :for f :in (repeat (length elements)
+                            (ensure-list for))
+         :collect
            (if (and (= f 1) (or (null element) (not (listp element))))
                element
                (cm:new cm:cycle :of element :for f)))
       (loop
-        :for element :in elements
-        :collect
+         :for element :in elements
+         :collect
            (if (or (null element) (not (listp element)))
                element
                (cm:new cm:cycle :of element)))))
 
 (defun make-war (elements &optional (for NIL for-p))
-  "Returns a LIST of weighted and/or symbols.
-   FOR >1 is useful to stutter ELEMENTS."
   (declare (type list elements))
   (if for-p
       (loop
-        :for element :in elements
-        :for f :in (ensure-list for)
-        :collect
-           (if (and (= f 1)
-                    (or (null element) (not (listp element))))
+         :for element :in elements
+         :for f :in (repeat (length elements)
+                            (ensure-list for))
+         :collect
+           (if (and (= f 1) (or (null element) (not (listp element))))
                element
-               (make-weighting element for)))
+               (cm:new cm:weighting :of element :for f)))
       (loop
-        :for element :in elements
-        :collect
+         :for element :in elements
+         :collect
            (if (or (null element) (not (listp element)))
                element
-               (make-weighting element)))))
+               (cm:new cm:weighting :of element)))))
+
+(defun make-har (elements &optional (for NIL for-p))
+  (declare (type list elements))
+  (if for-p
+      (loop
+         :for element :in elements
+         :for f :in (repeat (length elements)
+                            (ensure-list for))
+         :collect
+           (if (and (= f 1) (or (null element) (not (listp element))))
+               element
+               (cm:new cm:heap :of element :for f)))
+      (loop
+         :for element :in elements
+         :collect
+           (if (or (null element) (not (listp element)))
+               element
+               (cm:new cm:heap :of element)))))
 
 ;; Version that takes 1+N arguments
 (defgeneric make-cycles (for &rest rest)
@@ -169,11 +192,59 @@
                 rest)))
   (:method ((for list) &rest rest)
     (loop
-      :for f :in for
-      :for r :in rest :collect
+       :for f :in for
+       :for r :in rest :collect
          (if (= 1 f)
              r
              (cm:new cm:cycle
                :of (ensure-list r)
                :for f)))))
+
+;;--------------------------------------------------
+;; From SC
+(defun make-shuf (elements &optional (for NIL for-p))
+  "kind of Pshuf, shuffle the list in random order, and use
+   the same random order FOR times"
+  (if for-p
+      (cm:new cm:copier
+        :of (cm:new cm:heap :of elements)
+        :for for)
+      (cm:new cm:copier
+        :of (cm:new cm:heap :of elements))))
+
+;;--------------------------------------------------
+;; From nudruz/rhythm.lisp
+
+(defun upbeatcyc (minups maxups mindown maxdown)
+  "UPBEATCYC -- returns upbeat cycle
+   between 'minups' and 'maxups' upbeats of 1
+   between 'mindown' and 'maxdown' length of downbeat
+   > (next (upbeatcyc 2 5 10 20) 20)"
+  (cm:new cm:weighting
+    :of `((1 :min ,minups :max ,maxups)
+	  (,(pval (between mindown maxdown)) :max 1))))
+
+(defun cyc-rhythms (mlens subdivs)
+  "CYC-RHYTHMS -- combines measures and subdivisions cyclically
+   > (cyc-rhythms '(2 3 1.5) '(1 2))
+   (2.0 1.5 1.5 1.5 1.0 1.0 3.0 0.75 0.75)"
+  (let* ((subdivcyc (new cycle of subdivs))
+	 (mlencyc (new cycle of mlens)))
+    (loop :until (and (eop? subdivcyc)
+		      (eop? mlencyc))
+       :for this-subdiv = (next subdivcyc)
+       :for this-mlen = (next mlencyc)
+       :append (loop :repeat this-subdiv
+	          :collect (float (/ this-mlen this-subdiv))))))
+
+(defun combine-atks (rhythms atklist)
+  "COMBINE-ATKS -- 'fuses' attacks within a rhythm
+   'combiner' = number of attacks to combine (cycle)
+   > (combine-atks '(1 2) '(1 1 3))
+   (1 2 4 2 1 5)"
+  (let ((rcyc   (cm:new cm:cycle :of rhythms))
+	(atkcyc (cm:new cm:cycle :of atklist)))
+    (loop :until (and (eop? rcyc)
+                      (eop? atkcyc))
+       :collect (apply #'+ (next rcyc (next atkcyc))))))
 
