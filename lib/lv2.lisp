96fd7cdfbb34e8a604979cc75585f4f1bc2801d5
azimut <azimut.github@protonmail.com>
2019-03-01 21:20:13 -0300

    lv2: add play helpers

--- lib/lv2.lisp
+++ lib/lv2.lisp
@@ -3,11 +3,11 @@
 (dsp! swipe-dsp-up ((plugin (or null vug-foreign:plugin-instance))
                     (port-index fixnum)
                     dur
-                    max
-                    min)
+                    (max single-float)
+                    (min single-float))
   (:defaults NIL 45 1 1 0)
   (with-samples ((e (envelope
-                     (make-envelope `(,max ,min ,max) '(.4 .6))
+                     (make-envelope `(,max ,min ,max) '(.5 .5))
                      1 dur #'stop)))
     (foreach-frame
       (setf (f32-ref (vug-foreign:plugin-port-pointer plugin port-index))
@@ -16,12 +16,11 @@
 (dsp! swipe-dsp-down ((plugin (or null vug-foreign:plugin-instance))
                       (port-index fixnum)
                       dur
-                      max
-                      min)
-  (:defaults NIL 45 1
-             1 0)
+                      (max single-float)
+                      (min single-float))
+  (:defaults NIL 45 1 1 0)
   (with-samples ((e (envelope
-                     (make-envelope `(,max ,min ,max) '(.4 .6))
+                     (make-envelope `(,max ,min ,max) '(.5 .5))
                      1 dur #'stop)))
     (foreach-frame
       (setf (f32-ref (vug-foreign:plugin-port-pointer plugin port-index))
@@ -38,3 +37,33 @@
     (foreach-frame
       (setf (f32-ref (vug-foreign:plugin-port-pointer plugin port-index))
             (coerce (+ .5 (* .5 (sine 440 e))) 'single-float)))))
+
+;;--------------------------------------------------
+(defun lv2-note (time plugin keynum velocity duration)
+  (declare (type vug-foreign:plugin-instance plugin)
+           (type double-float time)
+           (type fixnum keynum velocity))
+  (rt-eval ()
+    (at time #'lv2:write-event plugin (lv2:midi-message #x90 keynum velocity))
+    (at (+ time (calc-beats duration))
+        (lambda (k) (lv2:write-event plugin (lv2:midi-message #x80 k 0)))
+        keynum))
+  keynum)
+(defun lv2-arp (time plugin keynum velocity duration)
+  (declare (type vug-foreign:plugin-instance plugin)
+           (type double-float time)
+           (type fixnum velocity)
+           (type list keynum))
+  (let* ((lnotes  (length keynum))
+         (offsets (loop :for i :from 0 :by duration :collect i :repeat lnotes)))
+    (mapcar (lambda (n o) (note-test (+ time (calc-beats o)) plugin n velocity duration))
+            keynum
+            offsets)))
+(defun lv2-control (plugin port-index value)
+  (declare (type vug-foreign:plugin-instance plugin)
+           (type fixnum port-index))
+  ;; Change the port value in rt-thread.
+  (rt-eval ()
+    (setf (f32-ref (vug-foreign:plugin-port-pointer plugin port-index))
+          (coerce value 'single-float)))
+  value)
